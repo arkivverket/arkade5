c2bc86eb5504819ddc0a641fe16a269824ee32b9
Henning Jensen <henning.jensen@gmail.com>
2016-07-04 11:27:40 +0200

    Add test project.

---
+++ src/Arkivverket.Arkade.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Arkivverket.Arkade.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Add(2, 2).Should().Be(4);
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+
+
+}
