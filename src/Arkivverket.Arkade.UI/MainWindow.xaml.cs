dfe288ee9af6117848ae5148047b90f9000758c1
Henning Jensen <henning.jensen@gmail.com>
2016-07-04 10:19:52 +0200

    Initial commit.

---
+++ src/Arkivverket.Arkade.UI/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Arkivverket.Arkade.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Title = Properties.Resources.General_WindowTitle;
+        }
+    }
+}
