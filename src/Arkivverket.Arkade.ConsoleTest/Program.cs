02dbe5a72784e1721cfca1d0481373b0980207d4
Henning Jensen <henning.jensen@gmail.com>
2016-07-12 16:00:42 +0200

    Add console test project.

---
+++ src/Arkivverket.Arkade.ConsoleTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arkivverket.Arkade.Core;
+using Arkivverket.Arkade.Identify;
+using Arkivverket.Arkade.Util;
+using Autofac;
+
+namespace Arkivverket.Arkade.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide file name for archive extraction (.tar).");
+                return;
+            }
+
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new ArkadeAutofacModule());
+            var container = builder.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                ArchiveExtractionReader archiveExtractionReader = container.Resolve<ArchiveExtractionReader>();
+
+
+                ArchiveExtraction archiveExtraction = archiveExtractionReader.ReadFromFile(args[0]);
+                Console.WriteLine($"Reading from archive: {args[0]}");
+                Console.WriteLine($"Uuid: {archiveExtraction.Uuid}");
+                Console.WriteLine($"WorkingDirectory: {archiveExtraction.WorkingDirectory}");
+            }
+
+            
+
+
+        }
+    }
+}
