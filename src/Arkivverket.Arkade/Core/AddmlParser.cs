5d2cb982184f17dcb334154353002e93fbcbf06a
Henning Jensen <henning.jensen@gmail.com>
2016-08-29 10:44:18 +0200

    Add ADDML schema and classes.

---
+++ src/Arkivverket.Arkade/Core/AddmlParser.cs
@@ -0,0 +1,9 @@
+namespace Arkivverket.Arkade.Core
+{
+    public class AddmlParser
+    {
+
+
+
+    }
+}
--- src/Arkivverket.Arkade/ExternalModels/xsd/GenerateClasses.bat
+++ src/Arkivverket.Arkade/ExternalModels/xsd/GenerateClasses.bat
@@ -1,10 +1,15 @@
 @echo off
 echo "============================ GENERATE C# classes ============================"
+
 echo "Generate classes for info.xsd"
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" /nologo info.xsd /c /n:Arkivverket.Arkade.ExternalModels.Info
 copy /y info.cs ..\Info.cs
 del info.cs
 
+echo "Generate classes for addml.xsd"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" /nologo addml.xsd /c /n:Arkivverket.Arkade.ExternalModels.Addml
+copy /y Addml.cs ..\Addml.cs
+del addml.cs
 
 
 pause
