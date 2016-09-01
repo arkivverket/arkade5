6f7c23903d053fdb53dc645a0f6adf49cedbdeb5
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2016-09-01 08:37:06 +0200

    Added random log message generator through Interface with event actions and Subscribe service

---
+++ src/Arkivverket.Arkade/LogInterface/ILogService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkivverket.Arkade.LogInterface
+{
+    public interface ILogService
+    {
+        void Subscribe();
+
+
+        event Action<LogEntry> LogMessageArrived;
+
+    }
+}
