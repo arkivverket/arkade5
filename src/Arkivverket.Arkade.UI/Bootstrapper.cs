742b9d9f4469a7aa97be61fa74534459be2c8e19
Henning Jensen <henning.jensen@gmail.com>
2016-09-08 09:50:36 +0200

    Remove old bootstrapper stuff from UI-project.

--- src/Arkivverket.Arkade.UI/Bootstrapper.cs
+++ src/Arkivverket.Arkade.UI/Bootstrapper.cs
@@ -31,47 +31,4 @@ namespace Arkivverket.Arkade.UI
             builder.RegisterModule(new ArkadeAutofacModule());
         }
     }
-
-
-    /*   public class Bootstrapper : UnityBootstrapper
-       {
-
-           protected override DependencyObject CreateShell()
-           {
-               return Container.Resolve<MainWindow>();
-           }
-
-           protected override void InitializeShell()
-           {
-               Application.Current.MainWindow.Show();
-           }
-
-           protected override void ConfigureContainer()
-           {
-               base.ConfigureContainer();
-               Container.RegisterTypeForNavigation<View000Debug>("View000Debug");
-               Container.RegisterTypeForNavigation<View100Status>("View100Status");
-
-               ILogService logService = new RandomLogService();
-               Container.RegisterInstance(logService);
-
-           }
-
-           protected override void ConfigureModuleCatalog()
-           {
-               ModuleCatalog catalog = (ModuleCatalog)ModuleCatalog;
-               catalog.AddModule(typeof(ModuleAModule));
-           }
-
-
-       }
-
-       public static class UnityExtensons
-       {
-           public static void RegisterTypeForNavigation<T>(this IUnityContainer container, string name)
-           {
-               container.RegisterType(typeof(object), typeof(T), name);
-           }
-       }
-       */
 }
