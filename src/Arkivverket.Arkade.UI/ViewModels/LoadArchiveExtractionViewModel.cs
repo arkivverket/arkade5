76ae648042afc80e1bb34d229371c6939da2f8bf
Henning Jensen <henning.jensen@gmail.com>
2016-09-09 12:28:05 +0200

    Run test engine on selected archive.

--- src/Arkivverket.Arkade.UI/ViewModels/LoadArchiveExtractionViewModel.cs
+++ src/Arkivverket.Arkade.UI/ViewModels/LoadArchiveExtractionViewModel.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Arkivverket.Arkade.Core;
+using Arkivverket.Arkade.Identify;
 using Microsoft.Win32;
 using Prism.Commands;
 using Prism.Mvvm;
@@ -9,44 +10,68 @@ namespace Arkivverket.Arkade.UI.ViewModels
 {
     public class LoadArchiveExtractionViewModel : BindableBase
     {
+        private readonly ArchiveExtractionReader _archiveExtractionReader;
         private readonly TestEngine _testEngine;
         private string _archiveFileName;
 
         private string _metadataFileName;
+        private bool _isRunningTests;
 
-
-        public LoadArchiveExtractionViewModel(TestEngine testEngine)
+        public LoadArchiveExtractionViewModel(ArchiveExtractionReader archiveExtractionReader, TestEngine testEngine)
         {
+            _archiveExtractionReader = archiveExtractionReader;
             _testEngine = testEngine;
             OpenMetadataFileCommand = new DelegateCommand(OpenMetadataFileDialog);
             OpenArchiveFileCommand = new DelegateCommand(OpenArchiveFileDialog);
-            RunTestEngineCommand = DelegateCommand.FromAsyncHandler(async () => await Task.Run(() => RunTests()));
+            RunTestEngineCommand = DelegateCommand.FromAsyncHandler(async () => await Task.Run(() => RunTests()), CanRunTests);
         }
 
         public string MetadataFileName
         {
             get { return _metadataFileName; }
-            set { SetProperty(ref _metadataFileName, value); }
+            set
+            {
+                SetProperty(ref _metadataFileName, value);
+                RunTestEngineCommand.RaiseCanExecuteChanged();
+            }
         }
 
         public string ArchiveFileName
         {
             get { return _archiveFileName; }
-            set { SetProperty(ref _archiveFileName, value); }
+            set
+            {
+                SetProperty(ref _archiveFileName, value);
+                RunTestEngineCommand.RaiseCanExecuteChanged();
+            }
         }
 
         public DelegateCommand RunTestEngineCommand { get; set; }
         public DelegateCommand OpenMetadataFileCommand { get; set; }
         public DelegateCommand OpenArchiveFileCommand { get; set; }
 
+        private bool CanRunTests()
+        {
+            return !_isRunningTests && !string.IsNullOrEmpty(_archiveFileName) && !string.IsNullOrEmpty(_metadataFileName);
+        }
+
         private void RunTests()
         {
             Debug.Print("Issued the RunTests command");
-            var workingDirectory = @"C:\temp\n5-alice-liten";
-            var archiveExtraction = new ArchiveExtraction("uuid", workingDirectory);
-            archiveExtraction.ArchiveType = ArchiveType.Noark5;
+            _isRunningTests = true;
+            RunTestEngineCommand.RaiseCanExecuteChanged();
 
-            //      _testEngine.RunTestsOnArchive(archiveExtraction);
+
+            ArchiveExtraction archive = _archiveExtractionReader.ReadFromFile(ArchiveFileName, MetadataFileName);
+
+            Debug.Print(archive.Uuid);
+            Debug.Print(archive.ArchiveType.ToString());
+            Debug.Print(archive.WorkingDirectory);
+
+            _testEngine.RunTestsOnArchive(archive);
+
+            _isRunningTests = false;
+            RunTestEngineCommand.RaiseCanExecuteChanged();
         }
 
         private void OpenMetadataFileDialog()
