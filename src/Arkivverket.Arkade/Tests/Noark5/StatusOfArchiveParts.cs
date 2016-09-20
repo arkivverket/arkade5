9c8c7ca9236b00b2ceed645359e32d7c21dba033
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2016-09-20 15:41:59 +0200

    Added tests for StatusOfArchiveParts

---
+++ src/Arkivverket.Arkade/Tests/Noark5/StatusOfArchiveParts.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Xml;
+using Arkivverket.Arkade.Core;
+using Arkivverket.Arkade.Util;
+
+namespace Arkivverket.Arkade.Tests.Noark5
+{
+    class StatusOfArchiveParts : BaseTest
+    {
+        public StatusOfArchiveParts() : base(TestType.Content)
+        {
+        }
+
+
+        protected override void Test(ArchiveExtraction archive)
+        {
+            TrackResults trackResults = new TrackResults();
+
+            using (var reader = XmlReader.Create(archive.GetContentDescriptionFileName()))
+            {
+                bool isSearchingForArchiveStatus = false;
+
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement("arkivdel"))
+                    {
+                        isSearchingForArchiveStatus = true;
+                    }
+                    else if (reader.IsNodeTypeAndName(XmlNodeType.Element, "arkivdelstatus") && isSearchingForArchiveStatus)
+                    {
+                        reader.Read(); // Adance the xml reader to the content of the node
+                        trackResults.Add(reader.Value);
+                        isSearchingForArchiveStatus = false;
+                    }
+                    else if (reader.IsNodeTypeAndName(XmlNodeType.EndElement, "arkivdelstatus") && isSearchingForArchiveStatus)
+                    {
+                        trackResults.Add("arkivdelstatus ikke definert i arkivdel");
+                        isSearchingForArchiveStatus = false;
+                    }
+                }
+            }
+
+            trackResults.ConsoleLogResults();
+            TestSuccess($"Found {trackResults.ResultsToString()} in archive.");
+        }
+    }
+
+
+    internal class TrackResults
+    {
+        private Dictionary<string, int> Results { get; set; }
+
+        internal TrackResults()
+        {
+            Results = new Dictionary<string, int>();
+        }
+
+        internal void Add(string resultEntry)
+        {
+            if (Results.ContainsKey(resultEntry))
+            {
+                Results[resultEntry] += 1;
+            }
+            else
+            {
+                Results.Add(resultEntry, 1);
+            }
+        }
+
+        internal void ConsoleLogResults()
+        {
+            foreach (var pair in Results)
+            {
+                Console.WriteLine($"arkivdelstatus: {pair.Key}: {pair.Value} obersvasjoner");
+            }
+        }
+
+
+        internal string ResultsToString()
+        {
+            string result = String.Empty;
+            foreach (var pair in Results)
+            {
+                result = result + $"{pair.Key} ({pair.Value}) ";
+            }
+            return result;
+        }
+
+
+
+    }
+
+}
---
+++ src/Arkivverket.Arkade/Util/XmlReaderHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml;
+
+namespace Arkivverket.Arkade.Util
+{
+    public static class XmlReaderHelpers
+    {
+
+        public static bool IsNodeTypeAndName(this XmlReader reader, XmlNodeType xmlNodeType, string name)
+        {
+            return (reader.NodeType == XmlNodeType.Element && reader.Name == name);
+        }
+    }
+}
