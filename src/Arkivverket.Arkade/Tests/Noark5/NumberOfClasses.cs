ead5d48278119acb320e6a5bc745064abfef09d9
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2016-09-21 14:48:08 +0200

    Added class for reading Noark5 classes (<klasse>)

---
+++ src/Arkivverket.Arkade/Tests/Noark5/NumberOfClasses.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml;
+using Arkivverket.Arkade.Core;
+using Arkivverket.Arkade.Util;
+
+namespace Arkivverket.Arkade.Tests.Noark5
+{
+    class NumberOfClasses : BaseTest
+    {
+        public NumberOfClasses() : base(TestType.Content)
+        {
+        }
+
+        protected override void Test(ArchiveExtraction archive)
+        {
+            using (var reader = XmlReader.Create(archive.GetContentDescriptionFileName()))
+            {
+                int counter = 0;
+
+                while (reader.Read())
+                {
+                    if (reader.IsNodeTypeAndName(XmlNodeType.Element, "klasse"))
+                    {
+                        counter++;
+                    }
+                }
+                Console.WriteLine("Number of classes: " + counter);
+                TestSuccess($"Found {counter} classes.");
+            }
+        }
+    }
+}
