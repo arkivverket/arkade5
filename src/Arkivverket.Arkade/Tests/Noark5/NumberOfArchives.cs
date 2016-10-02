5a0ae0f4ad899aafa281a1e0e0d26824b9ad2b7b
Henning Jensen <henning@arkitektum.no>
2016-10-02 22:10:24 +0200

    Fix typo in test message.

--- src/Arkivverket.Arkade/Tests/Noark5/NumberOfArchives.cs
+++ src/Arkivverket.Arkade/Tests/Noark5/NumberOfArchives.cs
@@ -24,7 +24,7 @@ namespace Arkivverket.Arkade.Tests.Noark5
 
                 AddAnalysisResult(AnalysisKeyArchives, counter.ToString());
 
-                TestSuccess($"Antall arkiver: {counter}.");
+                TestSuccess($"Antall arkiv: {counter}.");
             }
         }
 
