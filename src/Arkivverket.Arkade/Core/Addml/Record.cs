f8d0340653d9dcace1d0de6ff19619b7696e3abb
Jostein Tveit <jostein@arkitektum.no>
2016-10-18 11:55:06 +0200

    Added AddmlRecordDefinition to Record

--- src/Arkivverket.Arkade/Core/Addml/Record.cs
+++ src/Arkivverket.Arkade/Core/Addml/Record.cs
@@ -1,13 +1,17 @@
 using System.Collections.Generic;
+using Arkivverket.Arkade.Core.Addml.Definitions;
 
 namespace Arkivverket.Arkade.Core.Addml
 {
     public class Record
     {
+        public AddmlRecordDefinition Definition { private set; get; }
+
         public List<Field> Fields { private set; get; }
 
-        public Record(List<Field> fields)
+        public Record(AddmlRecordDefinition definition, List<Field> fields)
         {
+            Definition = definition;
             Fields = fields;
         }
     }
