3dfbef31a020fdfa9a523230e76f199d9e12313b
Jostein Tveit <jostein@arkitektum.no>
2016-10-25 08:07:56 +0200

    Noark4FileReader implemented

--- src/Arkivverket.Arkade/Core/Addml/Noark4FileReader.cs
+++ src/Arkivverket.Arkade/Core/Addml/Noark4FileReader.cs
@@ -1,17 +1,14 @@
 using System.Collections.Generic;
 using System.Xml;
-using System.Xml.Linq;
 using Arkivverket.Arkade.Core.Addml.Definitions;
 
 namespace Arkivverket.Arkade.Core.Addml
 {
     public class Noark4FileReader : IFlatFileReader
     {
-        private readonly XmlReader _reader;
+        private readonly XmlFormatReader _reader;
 
         private readonly AddmlRecordDefinition _addmlRecordDefinition;
-
-        private Record _nextRecord;
 
         public Noark4FileReader(FlatFile file)
         {
@@ -20,43 +17,31 @@ namespace Arkivverket.Arkade.Core.Addml
             // TODO: Support for multiple RecordDefinitions per file
             _addmlRecordDefinition = file.Definition.AddmlRecordDefinitions[0];
 
-            _reader = XmlReader.Create(filename);
-            _reader.MoveToContent();
+            XmlReader xmlReader = XmlReader.Create(filename);
+            string recordName = _addmlRecordDefinition.Name;
+
+            _reader = new XmlFormatReader(xmlReader, recordName);
         }
 
         public bool HasMoreRecords()
         {
-            string recordDefinitionName = _addmlRecordDefinition.Name;
-
-            while (_reader.Read())
-            {
-                if (_reader.NodeType == XmlNodeType.Element && _reader.Name == recordDefinitionName)
-                {
-                    XElement el = XNode.ReadFrom(_reader) as XElement;
-
-                    List<Field> fields = ParseFields(el);
-                    _nextRecord = new Record(_addmlRecordDefinition, fields);
-                    return true;
-                }
-            }
-
-            return false;
+            return _reader.HasNext();
         }
 
         public Record GetNextRecord()
         {
-            Record ret = _nextRecord;
-            _nextRecord = null;
-            return ret;
-        }
+            Dictionary<string, string> fieldValues = _reader.Next();
 
-        private List<Field> ParseFields(XElement node)
-        {
             List<Field> fields = new List<Field>();
+            foreach (AddmlFieldDefinition addmlFieldDefinition in _addmlRecordDefinition.AddmlFieldDefinitions)
+            {
+                string fieldName = addmlFieldDefinition.Name;
+                string fieldValue = fieldValues[fieldName];
 
-            // TODO: Use XmlFormatReader!
+                fields.Add(new Field(addmlFieldDefinition, fieldValue));
+            }
 
-            return fields;
+            return new Record(_addmlRecordDefinition, fields);
         }
     }
 }
