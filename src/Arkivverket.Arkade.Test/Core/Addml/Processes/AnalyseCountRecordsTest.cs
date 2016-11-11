f588cc43763d139ffa5b7d6a6d8bf75c0693c83a
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2016-11-11 15:36:49 +0100

    Added core ADDML process Analyse_CountRecords and test

---
+++ src/Arkivverket.Arkade.Test/Core/Addml/Processes/AnalyseCountRecordsTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arkivverket.Arkade.Core;
+using Arkivverket.Arkade.Core.Addml;
+using Arkivverket.Arkade.Core.Addml.Definitions;
+using Arkivverket.Arkade.Core.Addml.Processes;
+using Arkivverket.Arkade.Test.Core.Addml.Builders;
+using FluentAssertions;
+using Xunit;
+using Record = Arkivverket.Arkade.Core.Addml.Record;
+
+namespace Arkivverket.Arkade.Test.Core.Addml.Processes
+{
+    public class AnalyseCountRecordsTest
+    {
+        [Fact]
+        public void ShouldReportSuccessIfRecordCountIsCorrect()
+        {
+            AddmlFlatFileDefinition defintion = new AddmlFlatFileDefinitionBuilder()
+                .WithNumberOfRecords(4)
+                .WithFileName("filnavn.dat")
+                .Build();
+            FlatFile flatFile = new FlatFile(defintion);
+
+            AnalyseCountRecords test = new AnalyseCountRecords();
+            test.Run(flatFile);
+            test.Run((Arkade.Core.Addml.Record)null);
+            test.Run((Arkade.Core.Addml.Record)null);
+            test.Run((Arkade.Core.Addml.Record)null);
+            test.Run((Arkade.Core.Addml.Record)null);
+            test.EndOfFile();
+
+            TestRun testRun = test.GetTestRun();
+            testRun.IsSuccess().Should().BeTrue();
+            testRun.Results.Count.Should().Be(1);
+            testRun.Results[0].Message.Should().Be("RecordCount 4.");
+
+        }
+
+
+    }
+}
---
+++ src/Arkivverket.Arkade/Core/Addml/Processes/AnalyseCountRecords.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arkivverket.Arkade.Tests;
+
+namespace Arkivverket.Arkade.Core.Addml.Processes
+{
+    public class AnalyseCountRecords : IAddmlProcess
+    {
+
+        public const string Name = "Analyse_CountRecords";
+        private readonly TestRun _testRun;
+        private int _numberRecords;
+        private FlatFile _currentFlatFile;
+
+        public AnalyseCountRecords()
+        {
+            _testRun = new TestRun(Name, TestType.Content);
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void Run(FlatFile flatFile)
+        {
+            _numberRecords = 0;
+            _currentFlatFile = flatFile;
+        }
+
+        public void Run(Record record)
+        {
+            _numberRecords++;
+        }
+
+        public void Run(Field field)
+        {
+        }
+
+        public void EndOfFile()
+        {
+            _testRun.Add(new TestResult(ResultType.Success, new Location(_currentFlatFile.Definition.FileName), $"RecordCount {_numberRecords}."));
+        }
+
+        public TestRun GetTestRun()
+        {
+            return _testRun;
+        }
+    }
+}
