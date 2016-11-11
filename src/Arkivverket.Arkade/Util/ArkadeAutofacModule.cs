37f781c124e84f16c636cd62a75f959926480fa5
Henning Jensen <henning@arkitektum.no>
2016-11-11 14:56:35 +0100

    Make ConsoleTest runnable
    Missing reference in ArkadeAutofacModule caused trouble instatiating
    classes.

--- src/Arkivverket.Arkade/Util/ArkadeAutofacModule.cs
+++ src/Arkivverket.Arkade/Util/ArkadeAutofacModule.cs
@@ -2,6 +2,7 @@ using Arkivverket.Arkade.Core;
 using Arkivverket.Arkade.Identify;
 using Arkivverket.Arkade.Logging;
 using Arkivverket.Arkade.Tests;
+using Arkivverket.Arkade.Tests.Noark5;
 using Autofac;
 
 namespace Arkivverket.Arkade.Util
@@ -17,6 +18,7 @@ namespace Arkivverket.Arkade.Util
             builder.RegisterType<ArchiveContentReader>().As<IArchiveContentReader>();
             builder.RegisterType<TestProvider>().As<ITestProvider>();
             builder.RegisterType<StatusEventHandler>().As<IStatusEventHandler>().SingleInstance();
+            builder.RegisterType<Noark5TestProvider>().AsSelf();
         }
     }
 }
