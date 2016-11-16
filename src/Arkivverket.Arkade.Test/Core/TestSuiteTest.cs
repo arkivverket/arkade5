9c7b0f240a7faa6b77e70eb1aa8bb5ab8010b4ef
Henning Jensen <henning@arkitektum.no>
2016-11-16 13:55:57 +0100

    Add more tests to TestSuite

--- src/Arkivverket.Arkade.Test/Core/TestSuiteTest.cs
+++ src/Arkivverket.Arkade.Test/Core/TestSuiteTest.cs
@@ -7,18 +7,57 @@ namespace Arkivverket.Arkade.Test.Core
 {
     public class TestSuiteTest
     {
+        private readonly TestResult _testResultWithError = new TestResult(ResultType.Error, new Location(""), "feil");
+        private readonly TestResult _testResultWithSuccess = new TestResult(ResultType.Success, new Location(""), "feil");
 
         [Fact]
-        public void ShouldReturnOneError()
+        public void FindNumberOfErrorsShouldReturnOneError()
         {
             var testSuite = new TestSuite();
             var testRun = new TestRun("test with error", TestType.Content);
-            testRun.Add(new TestResult(ResultType.Error, new Location(""), "feil"));
+            testRun.Add(new TestResult(ResultType.Error, null, "feil"));
             testSuite.AddTestRun(testRun);
 
             testSuite.FindNumberOfErrors().Should().Be(1);
-
         }
 
+        [Fact]
+        public void FindNumberOfErrorsShouldReturnTwoErrors()
+        {
+            TestSuite testSuite = CreateTestSuite(_testResultWithError, _testResultWithError, _testResultWithSuccess);
+
+            testSuite.FindNumberOfErrors().Should().Be(2);
+        }
+
+        [Fact]
+        public void FindNumberOfErrorsShouldZeroErrorsWhenNoTestResults()
+        {
+            TestSuite testSuite = CreateTestSuite(null);
+
+            testSuite.FindNumberOfErrors().Should().Be(0);
+        }
+
+        [Fact]
+        public void FindNumberOfErrorsShouldZeroErrorsWhenOnlySuccessResults()
+        {
+            TestSuite testSuite = CreateTestSuite(_testResultWithSuccess, _testResultWithSuccess);
+
+            testSuite.FindNumberOfErrors().Should().Be(0);
+        }
+
+        private static TestSuite CreateTestSuite(params TestResult[] testResults)
+        {
+            var testSuite = new TestSuite();
+            var testRun = new TestRun("test with error", TestType.Content);
+            if (testResults != null)
+            {
+                foreach (var testResult in testResults)
+                {
+                    testRun.Add(testResult);
+                }
+            }
+            testSuite.AddTestRun(testRun);
+            return testSuite;
+        }
     }
 }
