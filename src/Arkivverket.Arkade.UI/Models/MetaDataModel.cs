29113e92037b93559aa01e5acd38d4ed4376bd16
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2016-12-14 16:49:59 +0100

    Initial work on metadata entry.

---
+++ src/Arkivverket.Arkade.UI/Models/MetaDataModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace Arkivverket.Arkade.UI.Models
+{
+    public class MetaDataModel : BindableBase  
+    {
+
+        private string _archiveDescription;
+        private string _agreementNumber;
+
+
+
+        public string ArchiveDescription
+        {
+            get { return _archiveDescription; }
+            set { SetProperty(ref _archiveDescription, value); }
+        }
+        public string AgreementNumber
+        {
+            get { return _agreementNumber; }
+            set { SetProperty(ref _agreementNumber, value); }
+        }
+
+
+        public MetaDataModel()
+        {
+            
+        }
+
+
+
+    }
+}
