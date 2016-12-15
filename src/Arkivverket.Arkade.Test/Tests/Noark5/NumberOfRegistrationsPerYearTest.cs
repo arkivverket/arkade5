af19a6dbfb95e30ab21023e2cc7797954f467150
Jørgen Tellnes <jorgen@arkitektum.no>
2016-12-15 14:05:44 +0100

    Add unit tests for noark5 test NumberOfRegistrationsPerYear

---
+++ src/Arkivverket.Arkade.Test/Tests/Noark5/NumberOfRegistrationsPerYearTest.cs
@@ -0,0 +1,76 @@
+﻿using Arkivverket.Arkade.Core;
+using Arkivverket.Arkade.Tests.Noark5;
+using FluentAssertions;
+using Xunit;
+
+namespace Arkivverket.Arkade.Test.Tests.Noark5
+{
+    public class NumberOfRegistrationsPerYearTest
+    {
+        [Fact]
+        public void ShouldReturnNumberOfRegistrationsForSingleYear()
+        {
+            XmlElementHelper helper = new XmlElementHelper().Add("arkiv",
+                new XmlElementHelper()
+                    .Add("arkivdel",
+                        new XmlElementHelper().Add("klassifikasjonssystem",
+                            new XmlElementHelper().Add("klasse",
+                                new XmlElementHelper().Add("mappe",
+                                    new XmlElementHelper().Add("registrering",
+                                        new XmlElementHelper().Add("opprettetDato", "1863-10-18T00:00:00Z")))))));
+
+            TestRun testRun = helper.RunEventsOnTest(new NumberOfRegistrationsPerYear());
+
+            testRun.Results[0].Message.Should().Contain("1863: 1");
+        }
+
+        [Fact]
+        public void ShouldReturnNumberOfRegistrationsSeveralYears()
+        {
+            XmlElementHelper helper = new XmlElementHelper().Add("arkiv",
+                new XmlElementHelper()
+                    .Add("arkivdel",
+                        new XmlElementHelper().Add("klassifikasjonssystem",
+                            new XmlElementHelper().Add("klasse",
+                                new XmlElementHelper()
+                                    .Add("mappe",
+                                        new XmlElementHelper().Add("registrering",
+                                            new XmlElementHelper().Add("opprettetDato", "1863-10-18T00:00:00Z")))
+                                    .Add("mappe",
+                                        new XmlElementHelper().Add("registrering",
+                                            new XmlElementHelper().Add("opprettetDato", "1863-10-18T00:00:00Z"))))))
+                    .Add("arkivdel",
+                        new XmlElementHelper().Add("klassifikasjonssystem",
+                            new XmlElementHelper().Add("klasse",
+                                new XmlElementHelper().Add("klasse",
+                                    new XmlElementHelper()
+                                        .Add("mappe",
+                                            new XmlElementHelper().Add("registrering",
+                                                new XmlElementHelper().Add("opprettetDato", "1864-10-18T00:00:00Z")))
+                                        .Add("mappe",
+                                            new XmlElementHelper().Add("registrering",
+                                                new XmlElementHelper().Add("opprettetDato", "1865-10-18T00:00:00Z"))))))));
+
+            TestRun testRun = helper.RunEventsOnTest(new NumberOfRegistrationsPerYear());
+
+            testRun.Results[0].Message.Should().Contain("1863: 2");
+            testRun.Results[1].Message.Should().Contain("1864: 1");
+            testRun.Results[2].Message.Should().Contain("1865: 1");
+        }
+
+        [Fact]
+        public void ShouldReturnNumberOfRegistrationsForNoYears()
+        {
+            XmlElementHelper helper = new XmlElementHelper().Add("arkiv",
+                new XmlElementHelper()
+                    .Add("arkivdel",
+                        new XmlElementHelper().Add("klassifikasjonssystem",
+                            new XmlElementHelper().Add("klasse",
+                                new XmlElementHelper().Add("mappe", new XmlElementHelper().Add("title", "Some title"))))));
+
+            TestRun testRun = helper.RunEventsOnTest(new NumberOfRegistrationsPerYear());
+
+            testRun.Results.Count.Should().Be(0);
+        }
+    }
+}
