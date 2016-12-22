4d2898791eeae65aeb1d737a03b832081a5c51e6
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2016-12-22 13:35:58 +0100

    Cleaned up spaces in code

--- src/Arkivverket.Arkade/Core/ArchiveMetadata.cs
+++ src/Arkivverket.Arkade/Core/ArchiveMetadata.cs
@@ -14,7 +14,7 @@ namespace Arkivverket.Arkade.Core
         public MetadataEntityInformationUnit Transferer { get; set; }
         public MetadataEntityInformationUnit Producer { get; set; }
         public List<MetadataEntityInformationUnit> Owner { get; set; }
-        public string  Recipient { get; set; }
+        public string Recipient { get; set; }
         public MetadataSystemInformationUnit System { get; set; }
         public MetadataSystemInformationUnit ArchiveSystem { get; set; }
         public List<string> Comments { get; set; }
