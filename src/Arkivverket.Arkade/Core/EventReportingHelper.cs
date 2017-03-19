b06d89c086850bc6f9e58c54c571963ff9b08005
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2017-03-19 23:46:34 +0100

    Added genreal function for event updates

---
+++ src/Arkivverket.Arkade/Core/EventReportingHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arkivverket.Arkade.Logging;
+
+namespace Arkivverket.Arkade.Core
+{
+    public class EventReportingHelper
+    {
+
+        private readonly IStatusEventHandler _statusEventHandler;
+
+        public EventReportingHelper(IStatusEventHandler statusEventHandler)
+        {
+            _statusEventHandler = statusEventHandler;
+        }
+
+
+        public void RaiseEventOperationMessage(string identifier, string message, OperationMessageStatus status)
+        {
+            _statusEventHandler.RaiseEventOperationMessage(identifier, message, status);
+        }
+
+    }
+}
