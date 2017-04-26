2e6c731468af37d322bd3260f1156f3f0c5dfed2
Jørgen Tellnes <jorgen@arkitektum.no>
2017-04-26 14:16:03 +0200

    Clean up code

--- src/Arkivverket.Arkade/Tests/Noark5/ControlDocumentFilesExists.cs
+++ src/Arkivverket.Arkade/Tests/Noark5/ControlDocumentFilesExists.cs
@@ -35,7 +35,6 @@ namespace Arkivverket.Arkade.Tests.Noark5
             return _testResults;
         }
 
-
         protected override void ReadStartElementEvent(object sender, ReadElementEventArgs eventArgs)
         {
         }
@@ -53,27 +52,23 @@ namespace Arkivverket.Arkade.Tests.Noark5
             if (eventArgs.Path.Matches("referanseDokumentfil"))
             {
                 string documentFileName = eventArgs.Value;
+
                 if (!FileExists(documentFileName))
                 {
                     _testResults.Add(new TestResult(ResultType.Error, new Location(documentFileName),
-                        Noark5Messages.ControlDocumentsFilesExistsMessage1));
+                        Noark5Messages.ControlDocumentsFilesExistsMessage1)
+                    );
                 }
             }
         }
 
-        /// <summary>
-        ///     Checks if file exists on disk.
-        ///     IMPORTANT - DO NOT PERFORM LOGGING ON EACH FILE EXISTS CHECK - IT KILLS PERFORMANCE!
-        /// </summary>
-        /// <param name="documentFileName"></param>
-        /// <returns></returns>
         private bool FileExists(string documentFileName)
         {
             try
             {
-                var file = new FileInfo(Path.Combine(_workingDirectory.FullName, documentFileName));
-                bool fileExists = file.Exists;
-                return fileExists;
+                var documentFileInfo = new FileInfo(Path.Combine(_workingDirectory.FullName, documentFileName));
+
+                return documentFileInfo.Exists; // Logging of exists-check greatly affects performance
             }
             catch
             {
