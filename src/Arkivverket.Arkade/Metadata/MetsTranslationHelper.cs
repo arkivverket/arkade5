3b9afd7ce4881f53dffcd4ad2b85b5f5061b9ee1
Jørgen Tellnes <jorgen@arkitektum.no>
2017-06-29 17:18:36 +0200

    Use local definition of ArchiveTypes to validate metadata systemtypes

--- src/Arkivverket.Arkade/Metadata/MetsTranslationHelper.cs
+++ src/Arkivverket.Arkade/Metadata/MetsTranslationHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Arkivverket.Arkade.Core;
 
 namespace Arkivverket.Arkade.Metadata
 {
@@ -6,20 +7,16 @@ namespace Arkivverket.Arkade.Metadata
     {
         public static bool IsValidSystemType(string systemType)
         {
-            // TODO: Use Enum ExternalModels.Mets.type (not ExternalModels.Info.type) when/if supported in built in mets schema
-
-            return Enum.IsDefined(typeof(ExternalModels.Info.type), systemType);
+            return Enum.IsDefined(typeof(ArchiveType), systemType);
         }
 
         public static bool IsSystemTypeNoark5(string systemType)
         {
-            // TODO: Use Enum ExternalModels.Mets.type (not ExternalModels.Info.type) when/if supported in built in mets schema
+            ArchiveType archiveType;
 
-            ExternalModels.Info.type enumSystemType;
+            bool isParsableSystemType = Enum.TryParse(systemType, out archiveType);
 
-            bool isParsableSystemType = Enum.TryParse(systemType, out enumSystemType);
-
-            return isParsableSystemType && enumSystemType == ExternalModels.Info.type.Noark5;
+            return isParsableSystemType && archiveType == ArchiveType.Noark5;
         }
     }
 }
