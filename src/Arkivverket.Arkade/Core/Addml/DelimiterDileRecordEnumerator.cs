4583ea8b8d8744837865e6a9bc0248744798c3db
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2017-07-08 19:44:00 +0200

    Added Shell of Enumerable record reader

---
+++ src/Arkivverket.Arkade/Core/Addml/DelimiterDileRecordEnumerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkivverket.Arkade.Core.Addml
+{
+    class DelimiterDileRecordEnumerator : IEnumerator
+    {
+
+        private StreamReader _stream;
+        private string _delimiter;
+
+        public DelimiterDileRecordEnumerator(StreamReader stream, string delimiter)
+        {
+            _stream = stream;
+            _delimiter = delimiter;
+        }
+
+    public object Current
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Arkivverket.Arkade/Core/Addml/DelimiterFileRecordEnumerable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkivverket.Arkade.Core.Addml
+{
+    class DelimiterFileRecordEnumerable : IEnumerable
+    {
+
+        private StreamReader _stream;
+        private String _delimiter;
+
+        public DelimiterFileRecordEnumerable(StreamReader stream, string delimiter)
+        {
+            _stream = stream;
+            _delimiter = delimiter;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new DelimiterDileRecordEnumerator(_stream, _delimiter);
+        }
+    }
+
+}
