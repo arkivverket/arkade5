83e57c1cfee2a30fc942eb8ff73ac88e83279272
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2017-08-09 13:02:29 +0200

    WIP implementing ADDML field Time

---
+++ src/Arkivverket.Arkade/Core/Addml/Definitions/DataTypes/TimeDataType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arkivverket.Arkade.Core.Addml.Definitions.DataTypes
+{
+    internal class TimeDataType : DataType
+    {
+/* 
+    <fieldType name="time"> 
+    <dataType>time</dataType> 
+    <fieldFormat>hhmmss</fieldFormat> 
+    </fieldType> 
+*/
+
+
+        public override bool IsValid(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
