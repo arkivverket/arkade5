7a84c2ca3dd9362b16d862eaff7b0f00b6afcf05
Henning Jensen <henning@arkitektum.no>
2017-08-09 11:13:22 +0200

    Improve sample console app
    Rewrite sample to use the new Arkade API.

--- src/Arkivverket.Arkade.ConsoleTest/Program.cs
+++ src/Arkivverket.Arkade.ConsoleTest/Program.cs
@@ -1,57 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
+using System.Reflection;
 using Arkivverket.Arkade.Core;
-using Arkivverket.Arkade.Core.Noark5;
-using Arkivverket.Arkade.Identify;
-using Arkivverket.Arkade.Util;
-using Autofac;
 using Serilog;
 
-namespace Arkivverket.Arkade.ConsoleTest
+namespace Sample.ConsoleApp
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            string directory = @"C:\Dropbox (Arkitektum AS)\Arkade5 - Testdata\Komplette uttrekk\Noark5-alice-liten\";
-            string archiveFileName = directory + @"ee07a0f9-4564-43ab-b80d-ac975ba12aed.tar";
-            string metadataFileName = directory + @"info.xml";
-            ArchiveType archiveType= ArchiveType.Noark5;
-            if (args.Length != 0)
-            {
-                archiveFileName = args[0];
-                metadataFileName = args[1];
-            }
-
-            var builder = new ContainerBuilder();
-            builder.RegisterModule(new ArkadeAutofacModule());
-            var container = builder.Build();
-
             Log.Logger = new LoggerConfiguration()
-                             .MinimumLevel.Debug()
-                             .WriteTo.ColoredConsole(outputTemplate: "{Timestamp:yyyy-MM-ddTHH:mm:ss.fff} {SourceContext} [{Level}] {Message}{NewLine}{Exception}")
-                             .CreateLogger();
+                .MinimumLevel.Information()
+                .WriteTo.ColoredConsole(
+                    outputTemplate: "{Timestamp:yyyy-MM-ddTHH:mm:ss.fff} {SourceContext} [{Level}] {Message}{NewLine}{Exception}")
+                .CreateLogger();
 
-            using (container.BeginLifetimeScope())
+            string archiveFileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "noark5-testuttrekk.tar");
+
+            var arkade = new Arkade();
+            TestSession session = arkade.RunTests(ArchiveFile.Read(archiveFileName, ArchiveType.Noark5));
+
+            foreach (TestRun testRun in session.TestSuite.TestRuns)
             {
-                TestSessionFactory testSessionBuilder = container.Resolve<TestSessionFactory>();
-                
-                TestSession testSession = testSessionBuilder.NewSession(ArchiveFile.Read(archiveFileName, archiveType));
-                Console.WriteLine($"Reading from archive: {archiveFileName}");
-                Console.WriteLine($"Uuid: {testSession.Archive.Uuid}");
-                Console.WriteLine($"WorkingDirectory: {testSession.Archive.WorkingDirectory}");
-                Console.WriteLine($"ArchiveType: {testSession.Archive.ArchiveType}");
-
-                TestEngineFactory testEngineFactory = container.Resolve<TestEngineFactory>();
-                ITestEngine testEngine = testEngineFactory.GetTestEngine(testSession);
-                TestSuite testSuite = testEngine.RunTestsOnArchive(testSession);
-                foreach (TestRun testRun in testSuite.TestRuns)
-                {
-                    Console.WriteLine($"Test: {testRun.TestName}, duration={testRun.TestDuration}, success={testRun.IsSuccess()}");
-                }
-
+                Log.Information($"Test name: {testRun.TestName}, duration={testRun.TestDuration}, success={testRun.IsSuccess()}");
             }
-
         }
     }
 }
