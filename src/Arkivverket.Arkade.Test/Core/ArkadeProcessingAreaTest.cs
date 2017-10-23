39b85866399ff9935ac4f4ef90675c3b4595bd1e
Jørgen Tellnes <jorgen@arkitektum.no>
2017-10-23 11:08:00 +0200

    Improve and un-skip tests

--- src/Arkivverket.Arkade.Test/Core/ArkadeProcessingAreaTest.cs
+++ src/Arkivverket.Arkade.Test/Core/ArkadeProcessingAreaTest.cs
@@ -7,32 +7,40 @@ using Xunit;
 
 namespace Arkivverket.Arkade.Test.Core
 {
-    public class ArkadeProcessingAreaTest
+    public class ArkadeProcessingAreaTest : IDisposable
     {
+        private readonly string _locationPath;
+        private readonly DirectoryInfo _location;
+
+        public ArkadeProcessingAreaTest()
+        {
+            _locationPath = Path.Combine(Environment.CurrentDirectory, "TestData", "ProcessingAreaTests");
+            _location = new DirectoryInfo(_locationPath);
+            _location.Create();
+        }
+
         [Fact]
         public void ProcessingAreaIsEstablished()
         {
-            string location = Path.Combine(Environment.CurrentDirectory, "TestData");
+            ArkadeProcessingArea.Establish(_locationPath);
 
-            ArkadeProcessingArea.Establish(location);
-
-            ArkadeProcessingArea.Location.FullName.Should().Be(location);
-            ArkadeProcessingArea.RootDirectory.FullName.Should().Be(location + "\\Arkade");
-            ArkadeProcessingArea.WorkDirectory.FullName.Should().Be(location + "\\Arkade\\work");
-            ArkadeProcessingArea.LogsDirectory.FullName.Should().Be(location + "\\Arkade\\logs");
+            ArkadeProcessingArea.Location.FullName.Should().Be(_locationPath);
+            ArkadeProcessingArea.RootDirectory.FullName.Should().Be(_locationPath + "\\Arkade");
+            ArkadeProcessingArea.WorkDirectory.FullName.Should().Be(_locationPath + "\\Arkade\\work");
+            ArkadeProcessingArea.LogsDirectory.FullName.Should().Be(_locationPath + "\\Arkade\\logs");
         }
 
-        [Fact (Skip="Fails on build server ...")]
+        [Fact]
         public void ProcessingAreaIsEstablishedWithInvalidLocation()
         {
-            string nonExistingLocation = Path.Combine(Environment.CurrentDirectory, "NonExistingDirectory");
+            string nonExistingLocation = Path.Combine(Environment.CurrentDirectory, "TestData", "NonExistingDirectory");
 
             ArkadeProcessingArea.Establish(nonExistingLocation);
 
             ProcessingAreaIsSetupWithTemporaryLogsDirectoryOnly();
         }
 
-        [Fact (Skip = "Fails on build server ...")]
+        [Fact]
         public void ProcessingAreaIsEstablishedWithMissingLocation()
         {
             ArkadeProcessingArea.Establish("");
@@ -52,6 +60,11 @@ namespace Arkivverket.Arkade.Test.Core
             ArkadeProcessingArea.Location.Should().BeNull();
             ArkadeProcessingArea.RootDirectory.Should().BeNull();
             ArkadeProcessingArea.WorkDirectory.Should().BeNull();
+        }
+
+        public void Dispose()
+        {
+            _location.Delete(true);
         }
     }
 }
