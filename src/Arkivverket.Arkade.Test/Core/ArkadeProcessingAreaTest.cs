38053ec3bb087ca61930c333ff1e2b830f717a03
Jørgen Tellnes <jorgen@arkitektum.no>
2017-10-26 13:20:24 +0200

    Revert "Add unit test for Arkade processing area cleanup routine"
    This reverts commit 1ff1f6b0ed8168efaa2cd1b25a00194167f5b734.

--- src/Arkivverket.Arkade.Test/Core/ArkadeProcessingAreaTest.cs
+++ src/Arkivverket.Arkade.Test/Core/ArkadeProcessingAreaTest.cs
@@ -30,57 +30,6 @@ namespace Arkivverket.Arkade.Test.Core
             ArkadeProcessingArea.LogsDirectory.FullName.Should().Be(_locationPath + "\\Arkade\\logs");
         }
 
-        [Fact]
-        public void ProcessingAreaIsCleanedUp()
-        {
-            ArkadeProcessingArea.Establish(_locationPath);
-
-            // CREATE SOME WORK-FILES:
-
-            // Create a file in the work-directory:
-            new FileInfo(Path.Combine(ArkadeProcessingArea.WorkDirectory.FullName, "someFile.txt")).Create().Close();
-            // Create a folder in the work-directory:
-            DirectoryInfo workDirectorySubDirectory = ArkadeProcessingArea.WorkDirectory.CreateSubdirectory("someFolder");
-            // Create a file in the sub-directory of the work-directory:
-            new FileInfo(Path.Combine(workDirectorySubDirectory.FullName, "someOtherFile.txt")).Create().Close();
-            // Confirm that the test-files have been created:
-            ArkadeProcessingArea.WorkDirectory.GetFiles("*", SearchOption.AllDirectories).Length.Should().Be(2);
-
-            // CREATE SOME LOG-FILES:
-
-            DateTime nowDate = DateTime.Now;
-            DateTime oldDate = DateTime.Now.AddDays(-7); // Date one week ago
-
-            string fileNameNewLog = $"arkade-{nowDate.Year}{nowDate.Month}{nowDate.Day}.log";
-            string fileNameOldLog = $"arkade-{oldDate.Year}{oldDate.Month}{oldDate.Day}.log";
-            string fileNameNewErrorLog = $"arkade-error-{nowDate.Year}{nowDate.Month}{nowDate.Day}235959.log";
-            string fileNameOldErrorLog = $"arkade-error-{oldDate.Year}{oldDate.Month}{oldDate.Day}235959.log";
-
-            // Create a new log file in the logs-directory:
-            new FileInfo(Path.Combine(ArkadeProcessingArea.LogsDirectory.FullName, fileNameNewLog)).Create().Close();
-            // Create an old log file in the logs-directory:
-            new FileInfo(Path.Combine(ArkadeProcessingArea.LogsDirectory.FullName, fileNameOldLog)).Create().Close();
-            // Create a new error log file in the logs-directory:
-            new FileInfo(Path.Combine(ArkadeProcessingArea.LogsDirectory.FullName, fileNameNewErrorLog)).Create().Close();
-            // Create an old error log file in the logs-directory:
-            new FileInfo(Path.Combine(ArkadeProcessingArea.LogsDirectory.FullName, fileNameOldErrorLog)).Create().Close();
-            // Confirm that the test-files have been created:
-            ArkadeProcessingArea.LogsDirectory.GetFiles().Length.Should().Be(4);
-            
-            // RUN CLEANUP AND INSPECT:
-
-            ArkadeProcessingArea.CleanUp();
-
-            // Work-directory are removed:
-            ArkadeProcessingArea.WorkDirectory.Exists.Should().BeFalse();
-            // Old logs are removed, new logs are kept:
-            ArkadeProcessingArea.LogsDirectory.GetFiles().Should().Contain(log => log.Name.Equals(fileNameNewLog));
-            ArkadeProcessingArea.LogsDirectory.GetFiles().Should().NotContain(log => log.Name.Equals(fileNameOldLog));
-            // Old error logs are removed, new error logs are kept:
-            ArkadeProcessingArea.LogsDirectory.GetFiles().Should().Contain(log => log.Name.Equals(fileNameNewErrorLog));
-            ArkadeProcessingArea.LogsDirectory.GetFiles().Should().NotContain(log => log.Name.Equals(fileNameOldErrorLog));
-        }
-
         [Fact (Skip="Failing on buildserver ...")]
         public void ProcessingAreaIsEstablishedWithInvalidLocation()
         {
