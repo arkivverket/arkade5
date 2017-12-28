7b36a759f25a661cee645465f964457f0e975d75
Jørgen Tellnes <jorgen@arkitektum.no>
2017-12-28 16:15:54 +0100

    Remove unused (duplicated) Noark 5 test

--- src/Arkivverket.Arkade/Resources/Noark5Messages.Designer.cs
+++ src/Arkivverket.Arkade/Resources/Noark5Messages.Designer.cs
@@ -1269,15 +1269,6 @@ namespace Arkivverket.Arkade.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Eventuelt - antall skjerminger i arkivstrukturen.
-        /// </summary>
-        internal static string NumberOfWithheldElements {
-            get {
-                return ResourceManager.GetString("NumberOfWithheldElements", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Finn status på arkivdeler.
         /// </summary>
         internal static string StatusOfArchiveParts {
