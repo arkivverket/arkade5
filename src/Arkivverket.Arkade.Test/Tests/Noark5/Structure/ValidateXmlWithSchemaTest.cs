67a9627f081499b580aa4ae1efc9b2b3cd4f3aaa
Dag Olav Dahle <dagolav@arkitektum.no>
2018-05-11 14:59:03 +0200

    Fix tests for validating xml with schema
    ARKADE-230

--- src/Arkivverket.Arkade.Test/Tests/Noark5/Structure/ValidateXmlWithSchemaTest.cs
+++ src/Arkivverket.Arkade.Test/Tests/Noark5/Structure/ValidateXmlWithSchemaTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Text;
 using Arkivverket.Arkade.Core;
+using Arkivverket.Arkade.Test.Core;
 using Arkivverket.Arkade.Tests;
 using Arkivverket.Arkade.Tests.Noark5.Structure;
 using FluentAssertions;
@@ -10,107 +11,45 @@ using Xunit.Abstractions;
 
 namespace Arkivverket.Arkade.Test.Tests.Noark5.Structure
 {
-    public class ValidateXmlWithSchemaTest : IDisposable
+    public class ValidateXmlWithSchemaTest
     {
-        public ValidateXmlWithSchemaTest(ITestOutputHelper outputHelper)
+        [Fact]
+        public void XmlFilesAreValidAccordingToSchema()
         {
-            _outputHelper = outputHelper;
-            _archiveContent = ValidContentStream();
-            _archiveStructureContent = ValidStructureStream();
+            string workingDirectory =
+                $"{AppDomain.CurrentDomain.BaseDirectory}\\TestData\\Noark5\\StructureValidation\\correct";
+
+            TestRun testRun = CreateTestRun(workingDirectory);
+
+            testRun.IsSuccess().Should().BeTrue();
         }
 
-        private Stream ValidStructureStream()
+        [Fact]
+        public void XmlFilesAreNotValidAccordingToSchema()
         {
-            var xml = @"<?xml version=""1.0"" encoding=""utf-8""?>" +
-                      @"<addml xmlns=""http://www.arkivverket.no/standarder/addml"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">" +
-                      @"<dataset><description>test description</description><reference></reference></dataset></addml>";
-            return GenerateStreamFromString(xml);
+            string workingDirectory =
+                $"{AppDomain.CurrentDomain.BaseDirectory}\\TestData\\Noark5\\StructureValidation\\error";
+
+            TestRun testRun = CreateTestRun(workingDirectory);
+
+            testRun.Results.Should().Contain(r => r.IsError());
         }
 
-        private Stream ValidContentStream()
+        private static TestRun CreateTestRun(string workingDirectory)
         {
-            var xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>" +
-                      @"<arkiv xmlns=""http://www.arkivverket.no/standarder/noark5/arkivstruktur"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">" +
-                      @"  <systemID/>" +
-                      @"  <tittel/>" +
-                      @"  <opprettetDato>2001-12-17T09:30:47Z</opprettetDato>" +
-                      @"  <opprettetAv/>" +
-                      @"  <avsluttetDato>2001-12-17T09:30:47Z</avsluttetDato>" +
-                      @"  <avsluttetAv/>" +
-                      @"  <arkivskaper>" +
-                      @"    <arkivskaperID/>" +
-                      @"    <arkivskaperNavn/>" +
-                      @"  </arkivskaper>" +
-                      @"  <arkivdel>" +
-                      @"    <systemID/>" +
-                      @"    <tittel/>" +
-                      @"    <arkivdelstatus>Aktiv periode</arkivdelstatus>" +
-                      @"    <opprettetDato>2001-12-17T09:30:47Z</opprettetDato>" +
-                      @"    <opprettetAv/>" +
-                      @"    <avsluttetDato>2001-12-17T09:30:47Z</avsluttetDato>" +
-                      @"    <avsluttetAv/>" +
-                      @"  </arkivdel>" +
-                      @"</arkiv>";
-            return GenerateStreamFromString(xml);
-        }
+            Archive archive = new ArchiveBuilder()
+                .WithArchiveType(ArchiveType.Noark5)
+                .WithWorkingDirectoryRoot(workingDirectory)
+                .WithWorkingDirectoryExternalContent(workingDirectory)
+                .Build();
 
-        public void Dispose()
-        {
-            _archiveStructureContent?.Dispose();
-            _archiveContent?.Dispose();
-        }
-
-        private readonly ITestOutputHelper _outputHelper;
-
-        private Stream _archiveStructureContent;
-        private Stream _archiveContent;
-
-        private TestRun RunTest()
-        {
-            Archive archive = new Core.ArchiveBuilder().WithWorkingDirectoryRoot("c:\\temp").Build();
             var validateXmlWithSchema = new ValidateXmlWithSchema();
+
             validateXmlWithSchema.Test(archive);
+
             return validateXmlWithSchema.GetTestRun();
         }
 
-        private MemoryStream GenerateStreamFromString(string value)
-        {
-            return new MemoryStream(Encoding.UTF8.GetBytes(value ?? ""));
-        }
 
-        [Fact(Skip = "Await archive files handling refactoring")]
-        public void ShouldReturnErrorsWhenAddmlXmlIsInvalidAccordingToSchema()
-        {
-            var xml =
-                @"<?xml version=""1.0"" encoding=""utf-8""?><addml xmlns=""http://www.arkivverket.no/standarder/addml""><hello></hello></addml>";
-            _archiveStructureContent = GenerateStreamFromString(xml);
-
-            RunTest().Results.Should().Contain(r => r.IsError() && r.Message.Contains("hello"));
-        }
-
-        [Fact(Skip = "Await archive files handling refactoring")]
-        public void ShouldReturnErrorsWhenArkivstrukturXmlIsInvalidAccordingToSchema()
-        {
-            var xml =
-                @"<?xml version=""1.0"" encoding=""utf-8""?><arkiv xmlns=""http://www.arkivverket.no/standarder/noark5/arkivstruktur""><hello></hello></arkiv>";
-            _archiveContent = GenerateStreamFromString(xml);
-
-            RunTest().Results.Should().Contain(r => r.IsError() && r.Message.Contains("hello"));
-        }
-
-        [Fact(Skip = "Interferred by test data from other tests ...")]
-        public void ShouldReturnSuccessWhenBothArkivuttrekkAndArkivstrukturIsValidXml()
-        {
-            var testResults = RunTest();
-            testResults.IsSuccess().Should().BeTrue();
-
-            foreach (var testResult in testResults.Results)
-            {
-                _outputHelper.WriteLine(
-                    (!string.IsNullOrEmpty(testResult.Location.ToString()) ? testResult.Location + ": " : string.Empty)
-                    + testResult.Message
-                );
-            }
-        }
     }
 }
