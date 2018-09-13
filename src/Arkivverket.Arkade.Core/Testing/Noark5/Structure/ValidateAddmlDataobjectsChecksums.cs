ac52e36808a59406c02f5dbbfa4d179a20385047
Unknown <jorgen@arkitektum.no>
2018-09-13 11:28:26 +0200

    Display mismatching checksums in equal case for easier visual comparison
    ARKADE-254

--- src/Arkivverket.Arkade.Core/Testing/Noark5/Structure/ValidateAddmlDataobjectsChecksums.cs
+++ src/Arkivverket.Arkade.Core/Testing/Noark5/Structure/ValidateAddmlDataobjectsChecksums.cs
@@ -74,7 +74,8 @@ namespace Arkivverket.Arkade.Core.Testing.Noark5.Structure
         {
             if (!checksumsAreEqual)
             {
-                var message = string.Format(Noark5Messages.ExceptionInvalidChecksum, fileName, expectedChecksum, generatedChecksum);
+                var message = string.Format(Noark5Messages.ExceptionInvalidChecksum, fileName,
+                    expectedChecksum.ToLower(), generatedChecksum.ToLower());
                 throw new ArkadeException(message);
             }
             return new TestResult(ResultType.Success, new Location(fileName), $"Sjekksum er kontrollert med algoritmen {checksumAlgorithm}.");
