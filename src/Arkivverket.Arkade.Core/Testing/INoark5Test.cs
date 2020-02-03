808e3c326a93ea96695ebef9f5f06e0245e3e0fb
Jørgen Tellnes <jorgen@arkitektum.no>
2020-02-03 16:13:48 +0100

    Refactor

--- src/Arkivverket.Arkade.Core/Testing/INoark5Test.cs
+++ src/Arkivverket.Arkade.Core/Testing/INoark5Test.cs
@@ -5,9 +5,9 @@ namespace Arkivverket.Arkade.Core.Testing
 {
     public interface INoark5Test : IArkadeTest
     {
-        void OnReadStartElementEvent(object sender, ReadElementEventArgs e);
-        void OnReadAttributeEvent(object sender, ReadElementEventArgs e);
-        void OnReadEndElementEvent(object sender, ReadElementEventArgs e);
+        void OnReadStartElementEvent(object sender, ReadElementEventArgs eventArgs);
+        void OnReadAttributeEvent(object sender, ReadElementEventArgs eventArgs);
         void OnReadElementValueEvent(object sender, ReadElementEventArgs eventArgs);
+        void OnReadEndElementEvent(object sender, ReadElementEventArgs eventArgs);
     }
 }
