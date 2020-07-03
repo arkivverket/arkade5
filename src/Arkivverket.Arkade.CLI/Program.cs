d1a98d366d42bd6724a77083ef0289b3d0904955
Leif Halvor <leif.halvor@arkitektum.no>
2020-07-03 12:00:22 +0200

    Improve error-messages given to user
    ARKADE-361

--- src/Arkivverket.Arkade.CLI/Program.cs
+++ src/Arkivverket.Arkade.CLI/Program.cs
@@ -23,44 +23,95 @@ namespace Arkivverket.Arkade.CLI
                 .WithNotParsed(LogParseErrors);
         }
 
-        public static ParserResult<object>ParseArguments(IEnumerable<string> args)
+        public static ParserResult<object> ParseArguments(IEnumerable<string> args)
         {
             return Parser.Default.ParseArguments<TestOptions, PackOptions, ProcessOptions, GenerateOptions>(args);
         }
 
         private static void RunProcessOptions(ProcessOptions processOptions)
         {
-            PrepareRun(processOptions);
+            if (!ReadyToRun(processOptions.OutputDirectory, processOptions.ProcessingArea, processOptions.MetadataFile,
+                processOptions.TestListFile))
+                return;
 
             CommandLineRunner.Run(processOptions);
         }
 
         private static void RunTestOptions(TestOptions testOptions)
         {
-            PrepareRun(testOptions);
-            
+            if (!ReadyToRun(testOptions.OutputDirectory, testOptions.ProcessingArea,
+                testListFilePath: testOptions.TestListFile))
+                return;
+
             CommandLineRunner.Run(testOptions);
         }
 
         private static void RunPackOptions(PackOptions packOptions)
         {
-            PrepareRun(packOptions);
+            if (!ReadyToRun(packOptions.OutputDirectory, packOptions.ProcessingArea, packOptions.MetadataFile))
+                return;
 
             CommandLineRunner.Run(packOptions);
         }
 
         private static void RunGenerateOptions(GenerateOptions generateOptions)
         {
-            PrepareRun(generateOptions);
+            if (!ReadyToRun(generateOptions))
+                return;
 
             CommandLineRunner.Run(generateOptions);
         }
 
-        private static void PrepareRun(Options options)
+        private static bool ReadyToRun(Options options)
         {
-            ArkadeProcessingArea.Establish(options.ProcessingArea); // Removes temporary log directory
+            return DirectoryArgsExists(options.OutputDirectory);
+        }
+
+        private static bool ReadyToRun(string outputDirectoryPath, string processingAreaPath,
+            string metadataFilePath = null, string testListFilePath = null)
+        {
+            if (!(DirectoryArgsExists(outputDirectoryPath, processingAreaPath) && FileArgsExists(metadataFilePath, testListFilePath)))
+                return false;
+
+            ArkadeProcessingArea.Establish(processingAreaPath); // Removes temporary log directory
 
             ConfigureLogging(); // Re-configured with log directory within processing area
+
+            return true;
+        }
+
+        private static bool DirectoryArgsExists(string outputDirectoryPath, string processingAreaPath = null)
+        {
+            if (!Directory.Exists(outputDirectoryPath))
+            {
+                Log.Error(new DirectoryNotFoundException(), $"Could not find output directory: '{outputDirectoryPath}'.");
+                return false;
+            }
+
+            if (processingAreaPath != null && !Directory.Exists(processingAreaPath))
+            {
+                Log.Error(new DirectoryNotFoundException(), $"Could not find processing area: '{processingAreaPath}'.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool FileArgsExists(string metadataFilePath = null, string testListFilePath = null)
+        {
+            if (metadataFilePath != null && !File.Exists(metadataFilePath))
+            {
+                Log.Error(new FileNotFoundException(), $"Could not find metadata file: '{metadataFilePath}'.");
+                return false;
+            }
+
+            if (testListFilePath != null && !File.Exists(testListFilePath))
+            {
+                Log.Error(new FileNotFoundException(), $"Could not find test list file: '{testListFilePath}'.");
+                return false;
+            }
+
+            return true;
         }
 
         private static void ConfigureLogging()
@@ -76,7 +127,7 @@ namespace Arkivverket.Arkade.CLI
                 .WriteTo.RollingFile(systemLogFilePath, outputTemplate: OutputStrings.SystemLogOutputTemplateForFile)
                 .CreateLogger();
         }
-        
+
         private static void LogParseErrors(IEnumerable<Error> errors)
         {
             foreach (Error error in errors)
