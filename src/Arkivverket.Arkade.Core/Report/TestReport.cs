db4b5e4261cfae42181babea0972854b09da1f27
Leif Halvor <leif.halvor@arkitektum.no>
2021-04-30 11:01:32 +0200

    Add generic test report model
    ARKADE-492

---
+++ src/Arkivverket.Arkade.Core/Report/TestReport.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Arkivverket.Arkade.Core.Base;
+using Arkivverket.Arkade.Core.Testing;
+
+namespace Arkivverket.Arkade.Core.Report
+{
+    public class TestReport
+    {
+        public TestReportSummary Summary { get; init; }
+        public List<ExecutedTest> TestsResults { get; init; }
+    }
+
+    public class TestReportSummary
+    {
+        public string Uuid { get; set; }
+        public ArchiveType ArchiveType { get; set; }
+        public string ArchiveCreators { get; set; }
+        public string ArchivalPeriod { get; set; }
+        public string SystemName { get; set; }
+        public string SystemType { get; set; }
+        public string DateOfTesting { get; set; }
+        public string NumberOfProcessedFiles { get; set; }
+        public string NumberOfProcessedRecords { get; set; }
+        public string NumberOfTestsRun { get; set; }
+        public string NumberOfErrors { get; set; }
+    }
+
+    public class ExecutedTest
+    {
+        public string TestId { get; set; }
+        public string TestName { get; set; }
+        public TestType TestType { get; set; }
+        public string TestDescription { get; set; }
+        public List<Result> TestResults { get; set; }
+    }
+
+    public class Result
+    {
+        public ResultType ResultType { get; set; }
+        public string Location { get; set; }
+        public string Message { get; set; }
+    }
+
+
+}
---
+++ src/Arkivverket.Arkade.Core/Report/TestReportFactory.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Arkivverket.Arkade.Core.Base;
+
+namespace Arkivverket.Arkade.Core.Report
+{
+    public static class TestReportFactory
+    {
+        public static TestReport Create(TestSession testSession)
+        {
+            var testReport = new TestReport
+            {
+                Summary = CreateTestReportSummary(testSession),
+                TestsResults = GetTestReportResults(testSession),
+            };
+
+            return testReport;
+        }
+
+        private static TestReportSummary CreateTestReportSummary(TestSession testSession)
+        {
+            var norwegianCulture = new CultureInfo("nb-NO");
+            int numberOfExecutedTests = testSession.TestSuite.TestRuns.Count();
+            int numberOfAvailableTests = testSession.AvailableTests.Count;
+
+            var summary = new TestReportSummary
+            {
+                Uuid = testSession.Archive.Uuid.ToString(),
+                ArchiveCreators = testSession.Archive.Details.ArchiveCreators,
+                ArchivalPeriod = testSession.Archive.Details.ArchivalPeriod,
+                SystemName = testSession.Archive.Details.SystemName,
+                SystemType = testSession.Archive.Details.SystemType,
+                ArchiveType = testSession.Archive.ArchiveType,
+                DateOfTesting = testSession.DateOfTesting.ToString(Resources.Report.DateFormat, norwegianCulture),
+                NumberOfTestsRun = string.Format(Resources.Report.ValueNumberOfTestsExecuted, numberOfExecutedTests, numberOfAvailableTests),
+                NumberOfProcessedFiles = testSession.TestSummary.NumberOfProcessedFiles,
+                NumberOfProcessedRecords = testSession.TestSummary.NumberOfProcessedRecords,
+                NumberOfErrors = testSession.TestSuite.FindNumberOfErrors().ToString(),
+            };
+
+            return summary;
+        }
+
+        private static List<ExecutedTest> GetTestReportResults(TestSession testSession)
+        {
+            var results = new List<ExecutedTest>();
+
+            foreach (TestRun testRun in testSession.TestSuite.TestRuns)
+            {
+                var test = new ExecutedTest
+                {
+                    TestId = testRun.TestId.ToString(),
+                    TestName = testRun.TestName,
+                    TestType = testRun.TestType,
+                    TestDescription = testRun.TestDescription,
+                    TestResults = new List<Result>(),
+                };
+
+                foreach (Result result in testRun.Results.Select(testRunResult =>
+                    new Result
+                    {
+                        ResultType = testRunResult.Result,
+                        Location = testRunResult.Location.ToString(),
+                        Message = testRunResult.Message,
+                    }))
+                {
+                    test.TestResults.Add(result);
+                }
+                results.Add(test);
+            }
+
+            return results;
+        }
+    }
+}
