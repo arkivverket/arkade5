68ce2798a187713b27cd93a72b7854857b3b8493
Jørgen Tellnes <jorgen@arkitektum.no>
2021-05-05 13:44:54 +0200

    Remove redundant code

--- src/Arkivverket.Arkade.Core/Base/Addml/Definitions/AddmlInfo.cs
+++ src/Arkivverket.Arkade.Core/Base/Addml/Definitions/AddmlInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.IO;
 using Arkivverket.Arkade.Core.ExternalModels.Addml;
-using Arkivverket.Arkade.Core.Util;
 
 namespace Arkivverket.Arkade.Core.Base.Addml.Definitions
 {
@@ -11,9 +10,6 @@ namespace Arkivverket.Arkade.Core.Base.Addml.Definitions
         
         public AddmlInfo(addml addml, FileInfo addmlFile)
         {
-            Assert.AssertNotNull(Resources.AddmlMessages.Addml, addml);
-            Assert.AssertNotNull(Resources.AddmlMessages.AddmlFile, addmlFile);
-
             Addml = addml;
             AddmlFile = addmlFile;
         }
