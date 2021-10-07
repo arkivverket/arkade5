6fc806b0f40c50a29c3860a019342a2bbc51d745
Leif Halvor <leif.halvor@arkitektum.no>
2021-10-07 10:59:08 +0200

    Only implementations of interface field was used

--- src/Arkivverket.Arkade.Core/Logging/ITestProgressReporter.cs
+++ src/Arkivverket.Arkade.Core/Logging/ITestProgressReporter.cs
@@ -4,8 +4,6 @@ namespace Arkivverket.Arkade.Core.Logging
 {
     public interface ITestProgressReporter
     {
-        bool IsRunning { get; }
-
         void Begin(ArchiveType archiveType);
         void ReportTestProgress(int testProgressValue);
         void Finish(bool hasFailed = false);
