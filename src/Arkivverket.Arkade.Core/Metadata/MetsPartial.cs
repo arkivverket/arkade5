25db414a69845d3eb374d0314e80b717e8189513
Leif Halvor <leif.halvor@arkitektum.no>
2022-03-02 12:26:37 +0100

    Amend descriptions of mets partial classes
    ARKADE-575

--- src/Arkivverket.Arkade.Core/Metadata/MetsPartial.cs
+++ src/Arkivverket.Arkade.Core/Metadata/MetsPartial.cs
@@ -1,12 +1,13 @@
 ﻿using System.Xml.Serialization;
 
-// ReSharper disable once CheckNamespace
 namespace Arkivverket.Arkade.Core.ExternalModels.DiasMets
 {
     // ReSharper disable once InconsistentNaming
-    // This class extends the mets class generated from an xsd. It adds the schemalocation attribute to the mets class. 
+    // This class extends the mets class generated from DIAS_METS.xsd. It adds the schemaLocation attribute to the mets class. 
     // This attribute will be appended to the output when the serializer is creating new mets files.
+#pragma warning disable IDE1006
     public partial class mets
+#pragma warning restore IDE1006
     {
         [XmlAttribute("schemaLocation", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
         public string SchemaLocation = "http://www.loc.gov/METS/ https://schema.arkivverket.no/METS/latest/DIAS_METS.xsd";
@@ -16,9 +17,11 @@ namespace Arkivverket.Arkade.Core.ExternalModels.DiasMets
 namespace Arkivverket.Arkade.Core.ExternalModels.SubmissionDescription
 {
     // ReSharper disable once InconsistentNaming
-    // This class extends the mets class generated from an xsd. It adds the schemalocation attribute to the mets class. 
+    // This class extends the mets class generated from submissionDescription.xsd. It adds the schemaLocation attribute to the mets class. 
     // This attribute will be appended to the output when the serializer is creating new mets files.
+#pragma warning disable IDE1006
     public partial class mets
+#pragma warning restore IDE1006
     {
         [XmlAttribute("schemaLocation", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
         public string SchemaLocation = "http://www.loc.gov/METS/ https://schema.arkivverket.no/INFOFIL/latest/submissionDescription.xsd";
