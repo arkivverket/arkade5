129b05a6b87b333174db302c8b5fd68c65d952fe
Jørgen Tellnes <jorgen@tellnes.nu>
2022-03-15 16:56:25 +0100

    Use block scoped namespace
    ARKADE-606

--- src/Arkivverket.Arkade.Core/Base/Addml/Definitions/AddmlDefinitionFlatFileName.cs
+++ src/Arkivverket.Arkade.Core/Base/Addml/Definitions/AddmlDefinitionFlatFileName.cs
@@ -1,21 +1,22 @@
-﻿namespace Arkivverket.Arkade.Core.Base.Addml.Definitions;
-
-public class AddmlDefinitionFlatFileName
+﻿namespace Arkivverket.Arkade.Core.Base.Addml.Definitions
 {
-    public string Path { get; }
-    public string Name { get; }
-    public string RelativeFilename { get; }
-    public string FullFileName => System.IO.Path.Combine(Path, Name);
-
-    public AddmlDefinitionFlatFileName(string path = "", string name = "", string relativeFilename="")
+    public class AddmlDefinitionFlatFileName
     {
-        Path = path;
-        Name = name;
-        RelativeFilename = relativeFilename;
-    }
+        public string Path { get; }
+        public string Name { get; }
+        public string RelativeFilename { get; }
+        public string FullFileName => System.IO.Path.Combine(Path, Name);
 
-    public override string ToString()
-    {
-        return FullFileName;
+        public AddmlDefinitionFlatFileName(string path = "", string name = "", string relativeFilename="")
+        {
+            Path = path;
+            Name = name;
+            RelativeFilename = relativeFilename;
+        }
+
+        public override string ToString()
+        {
+            return FullFileName;
+        }
     }
 }
--- src/Arkivverket.Arkade.Core/Util/ArchiveFormatValidation/ArchiveFormatValidationReport.cs
+++ src/Arkivverket.Arkade.Core/Util/ArchiveFormatValidation/ArchiveFormatValidationReport.cs
@@ -4,48 +4,49 @@ using Arkivverket.Arkade.Core.Resources;
 using static System.Environment;
 using static Arkivverket.Arkade.Core.Util.ArchiveFormatValidation.ArchiveFormatValidationResult;
 
-namespace Arkivverket.Arkade.Core.Util.ArchiveFormatValidation;
-
-public class ArchiveFormatValidationReport
+namespace Arkivverket.Arkade.Core.Util.ArchiveFormatValidation
 {
-    public readonly FileSystemInfo ValidatedItem;
-    public readonly ArchiveFormat ValidationFormat;
-    public readonly ArchiveFormatValidationResult ValidationResult;
-    public readonly string ValidationInfo;
-
-    public ArchiveFormatValidationReport(FileSystemInfo validatedItem, ArchiveFormat validationFormat,
-        ArchiveFormatValidationResult validationResult, string validationInfo = "")
+    public class ArchiveFormatValidationReport
     {
-        ValidationResult = validationResult;
-        ValidationFormat = validationFormat;
-        ValidatedItem = validatedItem;
-        ValidationInfo = validationInfo;
-    }
+        public readonly FileSystemInfo ValidatedItem;
+        public readonly ArchiveFormat ValidationFormat;
+        public readonly ArchiveFormatValidationResult ValidationResult;
+        public readonly string ValidationInfo;
 
-    public string ValidationSummary()
-    {
-        return ValidationResult switch
+        public ArchiveFormatValidationReport(FileSystemInfo validatedItem, ArchiveFormat validationFormat,
+            ArchiveFormatValidationResult validationResult, string validationInfo = "")
         {
-            Valid => string.Format(
-                ArchiveFormatValidationMessages.FileHasApprovedFormat, ValidatedItem.FullName, ValidationInfo
-            ),
-            Invalid => string.Format(
-                ArchiveFormatValidationMessages.FileHasNotApprovedFormat, ValidatedItem.FullName, ValidationInfo
-            ),
-            Error => string.Format(
-                ArchiveFormatValidationMessages.FileFormatValidationErrorMessage, ValidatedItem.FullName, ValidationInfo
-            ),
-            _ => throw new ArgumentOutOfRangeException($"No summary for {ValidationResult}")
-        };
-    }
+            ValidationResult = validationResult;
+            ValidationFormat = validationFormat;
+            ValidatedItem = validatedItem;
+            ValidationInfo = validationInfo;
+        }
 
-    public override string ToString()
-    {
-        return $@" Archive format validation report:{NewLine}{NewLine}"
-               + $@" Validated item: {ValidatedItem.FullName}{NewLine}"
-               + $@" Validation format: {ValidationFormat.GetDescription()}{NewLine}"
-               + $@" Validation result: {ValidationResult}{NewLine}"
-               + $@" Validation info: {ValidationInfo}{NewLine}"
-               + $@" Validation summary: {ValidationSummary()}{NewLine}";
+        public string ValidationSummary()
+        {
+            return ValidationResult switch
+            {
+                Valid => string.Format(
+                    ArchiveFormatValidationMessages.FileHasApprovedFormat, ValidatedItem.FullName, ValidationInfo
+                ),
+                Invalid => string.Format(
+                    ArchiveFormatValidationMessages.FileHasNotApprovedFormat, ValidatedItem.FullName, ValidationInfo
+                ),
+                Error => string.Format(
+                    ArchiveFormatValidationMessages.FileFormatValidationErrorMessage, ValidatedItem.FullName, ValidationInfo
+                ),
+                _ => throw new ArgumentOutOfRangeException($"No summary for {ValidationResult}")
+            };
+        }
+
+        public override string ToString()
+        {
+            return $@" Archive format validation report:{NewLine}{NewLine}"
+                   + $@" Validated item: {ValidatedItem.FullName}{NewLine}"
+                   + $@" Validation format: {ValidationFormat.GetDescription()}{NewLine}"
+                   + $@" Validation result: {ValidationResult}{NewLine}"
+                   + $@" Validation info: {ValidationInfo}{NewLine}"
+                   + $@" Validation summary: {ValidationSummary()}{NewLine}";
+        }
     }
 }
