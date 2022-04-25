f852a3cb961191f08425cef0c1f1d43ce54ae59b
Leif Halvor <leif.halvor@arkitektum.no>
2022-04-25 09:00:27 +0200

    Amend CLI options

--- src/Arkivverket.Arkade.CLI/ValidateOptions.cs
+++ src/Arkivverket.Arkade.CLI/ValidateOptions.cs
@@ -4,10 +4,13 @@ using CommandLine.Text;
 
 namespace Arkivverket.Arkade.CLI
 {
-    [Verb("validate-format", HelpText = "Validates the given file or directory against the specified format. Run this command followed by '--help' for more detailed info.")]
-    public class ValidateOptions : ValidateBaseOptions
+    [Verb("validate", HelpText = "Validates the given file or directory against the specified format. Run this command followed by '--help' for more detailed info.")]
+    public class ValidateOptions
     {
-        [Option('f', "format", HelpText = "The format which the file or directory is validated against. Available values: PDF/A, DIAS", Required = true)]
+        [Option('i', "item", HelpText = "The file or directory to be validated", Required = true)]
+        public string Item { get; set; }
+
+        [Option('f', "format", HelpText = "The format which the file or directory is validated against. Available values: PDF/A, DIAS SIP, DIAS AIP, DIAS SIP Noark5, DIAS AIP Noark5", Required = true)]
         public string Format { get; set; }
 
         [Usage(ApplicationAlias = "arkade")]
@@ -21,34 +24,14 @@ namespace Arkivverket.Arkade.CLI
                         Item = "/path/to/pdfA-file",
                         Format = "PDF/A"
                     });
-            }
-        }
-    }
 
-    [Verb("validate-dias", HelpText = "Validates that the given .tar or directory structurally fulfills the DIAS-specification")]
-    public class ValidateDiasOptions : ValidateBaseOptions
-    {
-        [Option('p', "information-package-type", HelpText = "Valid values: SIP, AIP.", Required = true)]
-        public string InformationPackageType { get; set; }
-
-        [Usage(ApplicationAlias = "arkade")]
-        public static IEnumerable<Example> Examples
-        {
-            get
-            {
-                yield return new Example("Structurally validate a specified tar-file against DIAS",
-                    new ValidateDiasOptions
+                yield return new Example("Validate a specified tar or directory to fulfill the DIAS standard",
+                    new ValidateOptions
                     {
-                        Item = "/path/to/tar-archive",
-                        InformationPackageType = "SIP"
+                        Item = "/path/to/dias-tar-or-directory",
+                        Format = "DIAS SIP"
                     });
             }
         }
-    }
-
-    public abstract class ValidateBaseOptions
-    {
-        [Option('i', "item", HelpText = "The file or directory to be validated", Required = true)]
-        public string Item { get; set; }
     }
 }
