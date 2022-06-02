69748f1fd0a19c72bfc08ad99ef1324cd06bec9e
Leif Halvor <leif.halvor@arkitektum.no>
2022-06-02 13:39:06 +0200

    Create CsvHelper util-class for writing csv-files

---
+++ src/Arkivverket.Arkade.Core/Util/CsvHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Arkivverket.Arkade.Core.Util
+{
+    public static class CsvHelper
+    {
+        public static void WriteToFile<T, TMap>(string filePath, IEnumerable<T> records) where TMap : ClassMap<T>
+        {
+            using var writer = new StreamWriter(filePath);
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            csv.Context.RegisterClassMap<TMap>();
+            csv.WriteRecords(records);
+        }
+
+        public static void WriteToFile<T>(string filePath, IEnumerable<T> records)
+        {
+            using var writer = new StreamWriter(filePath);
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            csv.WriteRecords(records);
+        }
+    }
+}
