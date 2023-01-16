29d530e925b438eda0d80df1b502ce69e98033b3
Leif Halvor <leif.halvor@arkitektum.no>
2023-01-16 15:16:43 +0100

    Make sure PDF/A validator is terminated on same conditions as Siegfried
    ARKADE-686

--- src/Arkivverket.Arkade.Core/Util/ExternalProcessManager.cs
+++ src/Arkivverket.Arkade.Core/Util/ExternalProcessManager.cs
@@ -1,4 +1,6 @@
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
@@ -8,11 +10,22 @@ namespace Arkivverket.Arkade.Core.Util
 {
     /// <summary>
     /// <para>Manages processes which are spawned from Arkade.</para>
+    /// <para>Third party applications (i.e. Siegfried, dbptk) should be managed with <see cref="Start"/> and
+    /// <see cref="Close(Process)"/>/<see cref="Close(int)"/> </para>
+    /// Processes spawned via third party libraries (i.e greenfield-apps from Codeuctivity's PDF/A validator) should
+    /// be managed with <see cref="Add"/> and <see cref="Remove"/>.
     /// </summary>
     public static class ExternalProcessManager
     {
         private static readonly ILogger Log = Serilog.Log.ForContext(MethodBase.GetCurrentMethod()?.DeclaringType);
         private static readonly Dictionary<int, Process> Processes = new();
+
+        private static readonly BackgroundWorker ProcessListener = new();
+
+        static ExternalProcessManager()
+        {
+            ProcessListener.DoWork += ProcessListenerOnDoWork;
+        }
 
         public static void Start(Process process)
         {
@@ -78,6 +91,48 @@ namespace Arkivverket.Arkade.Core.Util
         {
             Process process = Processes.Values.FirstOrDefault(p => !p.HasExited && p.ProcessName.Contains(processName));
             return process != default;
+        }
+
+        /// <summary>
+        /// Use when process is spawned from a third party library (I.e veraPDF - greenfield-apps.jar)
+        /// </summary>
+        /// <param name="processName"></param>
+        /// <param name="startTimeAfter"></param>
+        public static void Add(string processName, DateTime startTimeAfter)
+        {
+            ProcessListener.RunWorkerAsync(new object[]{processName, startTimeAfter});
+        }
+
+        /// <summary>
+        /// Use when process is spawned from a third party library (I.e veraPDF - greenfield-apps.jar)
+        /// </summary>
+        /// <param name="processName"></param>
+        public static void Remove(string processName)
+        {
+            var process = Processes.Values.FirstOrDefault(p => p.ProcessName.Contains(processName));
+            if (process != default)
+                Processes.Remove(process.Id);
+        }
+
+        private static void ProcessListenerOnDoWork(object sender, DoWorkEventArgs e)
+        {
+            var arguments = e.Argument as object[];
+
+            var processName = arguments[0] as string;
+
+            if (arguments[1] is not DateTime startTime)
+                return;
+
+            Process process = default;
+
+            while (process == default)
+            {
+                process = Process.GetProcesses().FirstOrDefault(p =>
+                    p.ProcessName.Contains(processName) &&
+                    p.StartTime.CompareTo(startTime) >= 0);
+            }
+
+            Processes.Add(process.Id, process);
         }
     }
 }
