1f988896ab477a895e6e85256bbbbd0d40a66ccd
Leif Halvor <leif.halvor@arkitektum.no>
2023-02-16 15:22:22 +0100

    WIP: Test extraction of separate files from tar-archive.

--- src/Arkivverket.Arkade.Core.Tests/Util/TarTests.cs
+++ src/Arkivverket.Arkade.Core.Tests/Util/TarTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using Arkivverket.Arkade.Core.Util;
 using FluentAssertions;
+using ICSharpCode.SharpZipLib.Tar;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -45,6 +48,52 @@ namespace Arkivverket.Arkade.Core.Tests.Util
             // Clean up temp directory
             System.IO.Directory.Delete(pathToExtractedDirectory.FullName, true);
             File.Delete(pathToTargetTarFile.FullName);
+        }
+
+        [Fact]
+        public void ExtractCertainFilesFromTarArchive()
+        {
+            var timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+            var pathSourceFolder = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory + "\\TestData\\Noark5\\AliceInWonderland\\");
+            var targetTarFile = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + $"\\TestData\\Noark5\\testSuiteArchive{timestamp}.tar");
+
+            _output.WriteLine("Source folder   : " + pathSourceFolder);
+            _output.WriteLine("Archived TAR    : " + targetTarFile);
+
+            new TarCompressionUtility().CompressFolderContentToArchiveFile(targetTarFile, pathSourceFolder);
+            _output.WriteLine("Generated Archive");
+
+            var tarEntryPositions = new Dictionary<string, TarEntryInfo>();
+
+            using (var tarInputStream = new TarInputStream(targetTarFile.OpenRead(), Encoding.UTF8))
+            {
+                while (tarInputStream.GetNextEntry() is { Name: { } } tarEntry)
+                {
+                    tarEntryPositions.Add(tarEntry.Name, new TarEntryInfo { Position = tarInputStream.Position, Size = tarEntry.Size });
+                }
+            }
+
+            TarEntryInfo arkivstrukturXmlEntryInfo = tarEntryPositions["arkivstrukturAbbreviated.xml"];
+            var arkivstrukturXmlAsStream = new byte[arkivstrukturXmlEntryInfo.Size];
+
+            using (FileStream tmpTarInputStream = targetTarFile.OpenRead())
+            {
+                tmpTarInputStream.Position = arkivstrukturXmlEntryInfo.Position;
+                int bytesRead = tmpTarInputStream.ReadAsync(arkivstrukturXmlAsStream, 0, (int)arkivstrukturXmlEntryInfo.Size).Result;
+            }
+
+            arkivstrukturXmlAsStream.Length.Should().Be((int)arkivstrukturXmlEntryInfo.Size);
+            //TODO: Check content for equality!!
+
+            // Clean up temp directory
+            File.Delete(targetTarFile.FullName);
+        }
+
+        private class TarEntryInfo
+        {
+            public long Position { get; set; }
+            public long Size { get; set; }
         }
     }
 }
