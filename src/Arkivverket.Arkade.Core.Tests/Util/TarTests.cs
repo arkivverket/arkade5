6c81d20fd1cba5d9ffa90c6ade9664b4063d4ac8
Leif Halvor <leif.halvor@arkitektum.no>
2023-02-22 16:59:58 +0100

    WIP: test read from tar as filestream.
    There are challenges with fetching the position from TarInputStream-object, because it reads in buffers. Therefore the position is not related to the entry.
    The tar must be read in blocks of 512 bytes. If the size of TarHeaders are known, it would be possible to skip blocks containing headers, while only extracting blocks containing files. More testing needed.

--- src/Arkivverket.Arkade.Core.Tests/Util/TarTests.cs
+++ src/Arkivverket.Arkade.Core.Tests/Util/TarTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using Arkivverket.Arkade.Core.Util;
@@ -65,44 +66,131 @@ namespace Arkivverket.Arkade.Core.Tests.Util
             _output.WriteLine("Generated Archive");
 
             var tarEntryPositions = new Dictionary<string, TarEntryInfo>();
-            Stream originalArkivstrukturStream = new MemoryStream();
+            using Stream originalArkivstrukturStream = new MemoryStream();
             using (var tarInputStream = new TarInputStream(targetTarFile.OpenRead(), Encoding.UTF8))
             {
-                while (tarInputStream.GetNextEntry() is { Name: { } } tarEntry)
+                var previousEntryName = "";
+                var previousEntrySize = 0L;
+                long numberOfEntries = 0;
+                TarEntry tarEntry;
+                var previousTarEntry = new TarEntry(new TarHeader());
+                while ((tarEntry = tarInputStream.GetNextEntry()) != null && tarEntry.Name != null)
                 {
-                    tarEntryPositions.Add(tarEntry.Name, new TarEntryInfo { Position = tarInputStream.Position, Size = tarEntry.Size });
+                    /*using var entryStream = new MemoryStream();
+                    tarInputStream.CopyEntryContents(entryStream);
+                    var checkSum = new Sha256ChecksumGenerator().GenerateChecksum(entryStream);*/
+                    //legg til sjekksum i dokumentfil-liste
+
+                    if (numberOfEntries > 0)
+                    {
+                        tarEntryPositions.Add(previousEntryName, new TarEntryInfo { Position = tarInputStream.Position - previousEntrySize+1, Size = previousEntrySize });
+                    }
+                    numberOfEntries++;
 
                     if (tarEntry.Name == "arkivstrukturAbbreviated.xml")
+                    {
+                        var available1 = tarInputStream.Available;
+                        Debug.WriteLine($"TarInputStream available: {tarInputStream.Available}");
+                        Debug.WriteLine($"TarInputStream Position: {tarInputStream.Position}");
                         tarInputStream.CopyEntryContents(originalArkivstrukturStream);
+                        var available2 = tarInputStream.Available;
+                        Debug.WriteLine($"TarInputStream available: {tarInputStream.Available}");
+                        Debug.WriteLine($"TarInputStream Position: {tarInputStream.Position}");
+                    }
+
+                    previousEntryName = tarEntry.Name;
+                    previousEntrySize = tarEntry.Size;
+                    previousTarEntry = tarEntry;
+                    var tarHeaderSize = tarEntry.TarHeader.Size;
                 }
+
+                if (previousTarEntry.Name != null)
+                {
+                    tarEntryPositions.Add(previousTarEntry.Name, new TarEntryInfo { Position = tarInputStream.Position, Size = previousTarEntry.Size });
+                }
+
+                Debug.WriteLine($"TarInputStream length: {tarInputStream.Length}");
             }
 
             TarEntryInfo arkivstrukturXmlEntryInfo = tarEntryPositions["arkivstrukturAbbreviated.xml"];
-            var arkivstrukturXmlAsStream = new byte[arkivstrukturXmlEntryInfo.Size];
+            var arkivstrukturXmlAsBytes = new List<byte>();
 
             using (FileStream tmpTarInputStream = targetTarFile.OpenRead())
             {
                 tmpTarInputStream.Position = arkivstrukturXmlEntryInfo.Position;
-                int bytesRead = tmpTarInputStream.ReadAsync(arkivstrukturXmlAsStream, 0, (int)arkivstrukturXmlEntryInfo.Size).Result;
+                while (tmpTarInputStream.Position < arkivstrukturXmlEntryInfo.Position + arkivstrukturXmlEntryInfo.Size)
+                {
+                    arkivstrukturXmlAsBytes.Add((byte)tmpTarInputStream.ReadByte());
+                }
+                Debug.WriteLine($"Tar as FileStream length: {tmpTarInputStream.Length}");
             }
 
             var originalBytes = new List<byte>();
             originalArkivstrukturStream.Position = 0;
+            var index = 0;
             while (true)
             {
                 var readByte = originalArkivstrukturStream.ReadByte();
 
+                //byte fetchedByte = arkivstrukturXmlAsBytes[index];
+
+                //if (fetchedByte != readByte)
+                //{
+                //    Debug.WriteLine($"At index '{index}': byte '{fetchedByte}' is not equal to original byte '{readByte}'");
+                //}
                 if (readByte == -1) break;
 
                 originalBytes.Add((byte)readByte);
+
+                index++;
             }
 
-            originalBytes.ToArray().Should().ContainInOrder(arkivstrukturXmlAsStream);
-            arkivstrukturXmlAsStream.Length.Should().Be((int)arkivstrukturXmlEntryInfo.Size);
+            //originalBytes.ToArray().Should().ContainInOrder(arkivstrukturXmlAsBytes);
+            arkivstrukturXmlAsBytes.Count.Should().Be((int)arkivstrukturXmlEntryInfo.Size);
             //TODO: Check content for equality!!
 
             // Clean up temp directory
             File.Delete(targetTarFile.FullName);
+        }
+
+        [Fact]
+        public void CompareByteContentOfTarFileTarInputStreamAndFileStream()
+        {
+            var timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+            var pathSourceFolder = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory + "\\TestData\\Noark5\\AliceInWonderland\\");
+            var targetTarFile = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + $"\\TestData\\Noark5\\testSuiteArchive{timestamp}.tar");
+
+            _output.WriteLine("Source folder   : " + pathSourceFolder);
+            _output.WriteLine("Archived TAR    : " + targetTarFile);
+
+            new TarCompressionUtility().CompressFolderContentToArchiveFile(targetTarFile, pathSourceFolder);
+            _output.WriteLine("Generated Archive");
+
+
+            var tarInputStreamAsBytes = new List<byte>();
+            using (var tarInputStream = new TarInputStream(targetTarFile.OpenRead(), Encoding.UTF8))
+            {
+                while (true)
+                {
+                    var readByte = tarInputStream.ReadByte();
+                    if (readByte == -1) break;
+                    tarInputStreamAsBytes.Add((byte)readByte);
+                }
+            }
+
+            var tarFileStreamAsBytes = new List<byte>();
+            using (var tarFileStream = targetTarFile.OpenRead())
+            {
+                while (true)
+                {
+                    var readByte = tarFileStream.ReadByte();
+                    if (readByte == -1) break;
+                    tarFileStreamAsBytes.Add((byte)readByte);
+                }
+            }
+
+            tarInputStreamAsBytes.Should().ContainInOrder(tarFileStreamAsBytes);
         }
 
         private class TarEntryInfo
