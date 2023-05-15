eb462e80b374f0b8a08534587426f4c272b811a7
Jørgen Tellnes <jorgen@arkitektum.no>
2023-05-15 09:56:20 +0200

    Ensure UUID is valid when read from string
    ARKADE-712

--- src/Arkivverket.Arkade.Core/Base/Uuid.cs
+++ src/Arkivverket.Arkade.Core/Base/Uuid.cs
@@ -1,36 +1,40 @@
-﻿using System;
+using System;
+using Arkivverket.Arkade.Core.Resources;
 
 namespace Arkivverket.Arkade.Core.Base
 {
     public class Uuid
     {
 
-        private readonly string _uuid;
+        private readonly Guid _uuid;
 
-        private Uuid(string uuid)
+        private Uuid(Guid uuid)
         {
             _uuid = uuid; // NB! UUID-transfer
         }
 
-        public static Uuid Of(string uuid)
+        public static Uuid Of(string uuidString)
         {
+            if (!Guid.TryParse(uuidString, out Guid uuid))
+                throw new ArkadeException(string.Format(ExceptionMessages.InvalidUUID, uuidString));
+
             return new Uuid(uuid);
         }
 
         public static Uuid Random()
         {
             Guid guid = Guid.NewGuid();
-            return new Uuid(guid.ToString());
+            return new Uuid(guid);
         }
 
         public string GetValue()
         {
-            return _uuid;
+            return _uuid.ToString();
         }
 
         public override string ToString()
         {
-            return _uuid;
+            return _uuid.ToString();
         }
 
         public override bool Equals(object obj)
