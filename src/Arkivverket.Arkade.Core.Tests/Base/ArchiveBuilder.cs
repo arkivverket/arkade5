a6ae891d37bbbec121a40e95b72a25b497dd43f2
Jørgen Tellnes <jorgen@arkitektum.no>
2023-06-12 14:50:35 +0200

    WIP

--- src/Arkivverket.Arkade.Core.Tests/Base/ArchiveBuilder.cs
+++ src/Arkivverket.Arkade.Core.Tests/Base/ArchiveBuilder.cs
@@ -15,7 +15,7 @@ namespace Arkivverket.Arkade.Core.Tests.Base
 
         public ArchiveBuilder WithUuid(string uuid)
         {
-            _uuid = Uuid.Of(uuid);
+            Uuid.TryParse(uuid, out _uuid);
             return this;
         }
 
