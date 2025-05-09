5fc38df9862de37636e8ac7bc0e9ece98b034a95
Jørgen Tellnes <jorgen@arkitektum.no>
2025-05-09 11:34:13 +0200

    Create ProcessingDirectory on demand

--- src/Arkivverket.Arkade.Core/Base/ArchiveProcessing.cs
+++ src/Arkivverket.Arkade.Core/Base/ArchiveProcessing.cs
@@ -3,26 +3,29 @@ using System.IO;
 
 namespace Arkivverket.Arkade.Core.Base;
 
-public class ArchiveProcessing
+public class ArchiveProcessing(Archive archive)
 {
-    public Archive Archive { get; }
-    public InputDiasPackage InputDiasPackage { get; }
-    public OutputDiasPackage OutputDiasPackage { get; set; }
-    public TestSession TestSession { get; set; }
-    public DirectoryInfo ProcessingDirectory { get; }
-
-    public ArchiveProcessing(Archive archive)
-    {
-        Archive = archive;
-
-        string workDirectory = ArkadeProcessingArea.WorkDirectory.FullName;
-        var nowTimeStamp = DateTime.Now.ToString("yyyyMMddHHmmss");
-        ProcessingDirectory = new DirectoryInfo(Path.Combine(workDirectory, nowTimeStamp));
-        ProcessingDirectory.Create();
-    }
-
     public ArchiveProcessing(InputDiasPackage inputDiasPackage) : this(inputDiasPackage.Archive)
     {
         InputDiasPackage = inputDiasPackage;
+    }
+
+    public Archive Archive { get; } = archive;
+    public InputDiasPackage InputDiasPackage { get; }
+    public OutputDiasPackage OutputDiasPackage { get; set; }
+    public TestSession TestSession { get; set; }
+    public DirectoryInfo ProcessingDirectory => _processingDirectory ?? CreateProcessingDirectory();
+
+    private DirectoryInfo _processingDirectory;
+
+    private DirectoryInfo CreateProcessingDirectory()
+    {
+        string workDirectoryFullName = ArkadeProcessingArea.WorkDirectory.FullName;
+        var nowTimeStampString = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+        _processingDirectory = new DirectoryInfo(Path.Combine(workDirectoryFullName, nowTimeStampString));
+        _processingDirectory.Create();
+
+        return _processingDirectory;
     }
 }
