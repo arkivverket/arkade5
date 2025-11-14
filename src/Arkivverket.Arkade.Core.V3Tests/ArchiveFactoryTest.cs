db2d25ccf67fb557a0dba913762995e0f8e85140
Jørgen Tellnes <jorgen@arkitektum.no>
2025-11-14 18:05:45 +0100

    Update test

--- src/Arkivverket.Arkade.Core.V3Tests/ArchiveFactoryTest.cs
+++ src/Arkivverket.Arkade.Core.V3Tests/ArchiveFactoryTest.cs
@@ -15,16 +15,24 @@ public class ArchiveFactoryTest
     {
         ArkadeProcessingArea.Establish(TestDataDirectory);
 
-        ValidateArchiveCreated(ArchiveType.Siard, typeof(SiardArchive), "Siard", "dbptk_produced.siard");
-        ValidateArchiveCreated(ArchiveType.Noark5, typeof(Noark5Archive), "Noark5", "Noark5Archive");
-        ValidateArchiveCreated(ArchiveType.Noark4, typeof(Noark4Archive));
-        ValidateArchiveCreated(ArchiveType.Noark3, typeof(AddmlArchive));
-        ValidateArchiveCreated(ArchiveType.Fagsystem, typeof(AddmlArchive));
-        
-        ArkadeProcessingArea.Destroy();
-    }
+        ValidateArchiveCreatedFromExtraction(ArchiveType.Siard, typeof(SiardArchive), "Siard", "dbptk_produced.siard");
+        ValidateArchiveCreatedFromExtraction(ArchiveType.Noark5, typeof(Noark5Archive), "Noark5", "Noark5Archive");
+        ValidateArchiveCreatedFromExtraction(ArchiveType.Noark4, typeof(Noark4Archive));
+        ValidateArchiveCreatedFromExtraction(ArchiveType.Noark3, typeof(AddmlArchive));
+        ValidateArchiveCreatedFromExtraction(ArchiveType.Fagsystem, typeof(AddmlArchive));
 
-    private static void ValidateArchiveCreated(ArchiveType archiveType, Type expectedType, params string[] testDataPathSegments)
+        ValidateArchiveCreatedFromDiasPackage(ArchiveType.Siard, typeof(SiardArchive),
+            "Siard", "Siard-utrekk_som_SIP", "e27fdbc9-93f7-46ad-865c-66699316400d.tar");
+        ValidateArchiveCreatedFromDiasPackage(ArchiveType.Noark5, typeof(Noark5Archive),
+            "Noark5", "Noark5_liten_AIP", "c0ada300-e5f2-48f2-8ad6-4303a28ff2d2.tar");
+        // ValidateArchiveCreatedFromDiasPackage(ArchiveType.Noark4, typeof(Noark4Archive)); // Provide test-data (SIP/AIP)
+        // ValidateArchiveCreatedFromDiasPackage(ArchiveType.Noark3, typeof(AddmlArchive)); // Provide test-data (SIP/AIP)
+        // ValidateArchiveCreatedFromDiasPackage(ArchiveType.Fagsystem, typeof(AddmlArchive)); // Provide test-data (SIP/AIP)
+
+        // ArkadeProcessingArea.Destroy(); // Release resources so that this can be destroyed.
+    }
+    
+    private static void ValidateArchiveCreatedFromExtraction(ArchiveType archiveType, Type expectedType, params string[] testDataPathSegments)
     {
         string testDataPath = Path.Combine(TestDataDirectory, Path.Combine(testDataPathSegments));
 
@@ -33,5 +41,26 @@ public class ArchiveFactoryTest
             : ArchiveFactory.Create(new DirectoryInfo(testDataPath), archiveType);
 
         archive.GetType().Should().Be(expectedType);
+        archive.ArchiveType.Should().Be(archiveType); // Get rid of
+        archive.InputDiasPackage.Should().BeNull();
+    }
+
+    private static void ValidateArchiveCreatedFromDiasPackage(ArchiveType archiveType, Type expectedType, params string[] testDataPathSegments)
+    {
+        string testDataPath = Path.Combine(TestDataDirectory, Path.Combine(testDataPathSegments));
+    
+        Archive archive;
+        if (Path.HasExtension(testDataPath))
+        {
+            archive = ArchiveFactory.Create(new FileInfo(testDataPath), archiveType);
+        }
+        else
+        {
+            archive = ArchiveFactory.Create(new DirectoryInfo(testDataPath), archiveType);
+        }
+
+        archive.GetType().Should().Be(expectedType);
+        archive.ArchiveType.Should().Be(archiveType); // Get rid of
+        archive.InputDiasPackage.Should().NotBeNull();
     }
 }
