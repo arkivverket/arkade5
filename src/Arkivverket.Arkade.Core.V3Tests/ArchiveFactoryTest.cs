393ebe77923f03a23b97f48852060afb33ebabe9
Jørgen Tellnes <jorgen@arkitektum.no>
2025-11-14 17:03:36 +0100

    Update unit test

--- src/Arkivverket.Arkade.Core.V3Tests/ArchiveFactoryTest.cs
+++ src/Arkivverket.Arkade.Core.V3Tests/ArchiveFactoryTest.cs
@@ -7,19 +7,31 @@ namespace Arkivverket.Arkade.Core.V3Tests;
 
 public class ArchiveFactoryTest
 {
+    private static readonly string TestDataDirectory = Path.Combine(Environment.CurrentDirectory, "TestData");
+    private static readonly ArchiveFactory ArchiveFactory = new(new TarCompressionUtility(), new StatusEventHandler());
+
     [Fact]
     public void CreateTest()
     {
-        CallWith(new DirectoryInfo(""), ArchiveType.Siard).GetType().Should().Be(typeof(SiardArchive));
-        CallWith(new DirectoryInfo(""), ArchiveType.Noark5).GetType().Should().Be(typeof(Noark5Archive));
-        CallWith(new DirectoryInfo(""), ArchiveType.Noark4).GetType().Should().Be(typeof(Noark4Archive));
-        CallWith(new DirectoryInfo(""), ArchiveType.Noark3).GetType().Should().Be(typeof(AddmlArchive));
-        CallWith(new DirectoryInfo(""), ArchiveType.Fagsystem).GetType().Should().Be(typeof(AddmlArchive));
+        ArkadeProcessingArea.Establish(TestDataDirectory);
+
+        ValidateArchiveCreated(ArchiveType.Siard, typeof(SiardArchive), "Siard", "dbptk_produced.siard");
+        ValidateArchiveCreated(ArchiveType.Noark5, typeof(Noark5Archive), "Noark5", "Noark5Archive");
+        ValidateArchiveCreated(ArchiveType.Noark4, typeof(Noark4Archive));
+        ValidateArchiveCreated(ArchiveType.Noark3, typeof(AddmlArchive));
+        ValidateArchiveCreated(ArchiveType.Fagsystem, typeof(AddmlArchive));
+        
+        ArkadeProcessingArea.Destroy();
     }
 
-    private static Archive CallWith(FileSystemInfo archiveSource, ArchiveType archiveType)
+    private static void ValidateArchiveCreated(ArchiveType archiveType, Type expectedType, params string[] testDataPathSegments)
     {
-        return new ArchiveFactory(new TarCompressionUtility(), new StatusEventHandler())
-            .Create(archiveSource, archiveType);
+        string testDataPath = Path.Combine(TestDataDirectory, Path.Combine(testDataPathSegments));
+
+        Archive archive = Path.HasExtension(testDataPath)
+            ? ArchiveFactory.Create(new FileInfo(testDataPath), archiveType)
+            : ArchiveFactory.Create(new DirectoryInfo(testDataPath), archiveType);
+
+        archive.GetType().Should().Be(expectedType);
     }
 }
