90336c5695e7b1758c4c5082a5a97adb56d1581f
Jørgen Tellnes <jorgen@arkitektum.no>
2025-12-03 13:00:45 +0100

    Add FileArchiveContent unit tests

---
+++ src/Arkivverket.Arkade.Core.V3Tests/FileArchiveContentTest.cs
@@ -0,0 +1,27 @@
+using Arkivverket.Arkade.Core.Base;
+using FluentAssertions;
+
+namespace Arkivverket.Arkade.Core.V3Tests;
+
+public class FileArchiveContentTest
+{
+    private static readonly FileInfo TestFile = new(
+        Path.Combine(Environment.CurrentDirectory, "TestData", "Archives", "Siard", "extraction", "dbptk.siard"));
+
+    private readonly FileArchiveContent _content = new(TestFile);
+
+    [Fact]
+    public void RootFile_ShouldBeTheProvidedFile()
+    {
+        _content.RootFile.Should().Be(TestFile);
+    }
+
+    [Fact]
+    public void GetAllContentsTest()
+    {
+        FileSystemInfo[] allContents = _content.GetAllContents().ToArray();
+
+        allContents.Should().HaveCount(1);
+        allContents[0].Should().Be(_content.RootFile);
+    }
+}
