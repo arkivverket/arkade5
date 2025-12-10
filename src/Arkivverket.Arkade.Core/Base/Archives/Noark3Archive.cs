ba6b634c8df899bb0c29b708754c0e4724abbda4
Jørgen Tellnes <jorgen@arkitektum.no>
2025-12-10 15:15:31 +0100

    Refactor

--- src/Arkivverket.Arkade.Core/Base/Archives/Noark3Archive.cs
+++ src/Arkivverket.Arkade.Core/Base/Archives/Noark3Archive.cs
@@ -2,6 +2,5 @@ using System.IO;
 
 namespace Arkivverket.Arkade.Core.Base.Archives;
 
-public class Noark3Archive(
-    DirectoryArchiveContent content, DirectoryInfo processingDirectory, InputDiasPackage diasPackage = null)
+public class Noark3Archive(DirectoryArchiveContent content, DirectoryInfo processingDirectory, InputDiasPackage diasPackage = null)
     : AddmlDefinitionTestedArchive(content, processingDirectory, diasPackage);
--- src/Arkivverket.Arkade.Core/Base/Archives/Noark4Archive.cs
+++ src/Arkivverket.Arkade.Core/Base/Archives/Noark4Archive.cs
@@ -3,13 +3,9 @@ using Arkivverket.Arkade.Core.Resources;
 
 namespace Arkivverket.Arkade.Core.Base.Archives;
 
-public class Noark4Archive : Archive
+public class Noark4Archive(DirectoryArchiveContent content, DirectoryInfo processingDirectory, InputDiasPackage inputDiasPackage = null)
+    : Archive(content, processingDirectory, inputDiasPackage)
 {
-    public Noark4Archive(DirectoryArchiveContent content, DirectoryInfo processingDirectory, InputDiasPackage inputDiasPackage = null)
-        : base(content, processingDirectory, inputDiasPackage)
-    {
-    }
-
     public override bool IsTestable(out string disqualifyingCause)
     {
         disqualifyingCause = Messages.Noark4ValidationNotSupported;
