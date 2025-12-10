4c45c314b6fdcb59843333be53e86eee3f8b942a
Jørgen Tellnes <jorgen@arkitektum.no>
2025-12-10 15:13:59 +0100

    Remove sealed from class

--- src/Arkivverket.Arkade.Core/Base/Archives/SpecializedSystemArchive.cs
+++ src/Arkivverket.Arkade.Core/Base/Archives/SpecializedSystemArchive.cs
@@ -2,5 +2,5 @@ using System.IO;
 
 namespace Arkivverket.Arkade.Core.Base.Archives;
 
-public sealed class SpecializedSystemArchive(DirectoryArchiveContent content, DirectoryInfo processingDirectory, InputDiasPackage inputDiasPackage = null)
+public class SpecializedSystemArchive(DirectoryArchiveContent content, DirectoryInfo processingDirectory, InputDiasPackage inputDiasPackage = null)
     : AddmlDefinitionTestedArchive(content, processingDirectory, inputDiasPackage);
