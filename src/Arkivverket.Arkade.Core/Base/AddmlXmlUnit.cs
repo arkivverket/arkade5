575612d2d66c634be720d5fe3bac5fb288db8833
Jørgen Tellnes <jorgen@arkitektum.no>
2026-01-06 11:10:10 +0100

    Remove unused methods

--- src/Arkivverket.Arkade.Core/Base/AddmlXmlUnit.cs
+++ src/Arkivverket.Arkade.Core/Base/AddmlXmlUnit.cs
@@ -3,14 +3,5 @@ namespace Arkivverket.Arkade.Core.Base;
 public class AddmlXmlUnit(ArchiveXmlFile archiveXmlFile, ArchiveXmlSchema archiveXmlSchema)
     : ArchiveXmlUnit(archiveXmlFile, [archiveXmlSchema])
 {
-    public ArchiveXmlSchema Schema
-    {
-        get => Schemas[0];
-        set => Schemas[0] = value;
-    }
-
-    internal bool HasNoDefinedSchema()
-    {
-        return Schema == null;
-    }
+    public ArchiveXmlSchema Schema => Schemas[0];
 }
