83a9ec8fc86c413c1e7c9f03139d5edc080024c7
Jørgen Tellnes <jorgen@arkitektum.no>
2026-01-15 10:24:06 +0100

    Test for metadata-package-consistency

--- src/Arkivverket.Arkade.Core.Tests/Base/InformationPackageCreatorTest.cs
+++ src/Arkivverket.Arkade.Core.Tests/Base/InformationPackageCreatorTest.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Arkivverket.Arkade.Core.Base;
 using Arkivverket.Arkade.Core.Base.Archives;
@@ -33,7 +34,8 @@ public class InformationPackageCreatorTest
     [Trait("Category", "Integration")]
     public void CreateSipTest()
     {
-        (Uuid outputPackageId, List<string> packageFileList) = CreatePackage(PackageType.SubmissionInformationPackage);
+        (Uuid outputPackageId, List<string> metadataFileList, List<string> packageFileList) =
+            CreatePackage(PackageType.SubmissionInformationPackage);
 
         string rootDir = outputPackageId + "/";
 
@@ -65,13 +67,18 @@ public class InformationPackageCreatorTest
 
         // Files in total, including the directories implicit tested by subentries (above)
         packageFileList.Count.Should().Be(19);
+
+        // All files in the package (except the metadata file itself) should be described in its metadata:
+        List<string> packageFilesExpectedInMetadata = GetPackageItemsExpectedInMetadata(packageFileList);
+        metadataFileList.Should().BeEquivalentTo(packageFilesExpectedInMetadata);
     }
 
     [Fact]
     [Trait("Category", "Integration")]
     public void CreateAipTest()
     {
-        (Uuid outputPackageId, List<string> packageFileList) = CreatePackage(PackageType.ArchivalInformationPackage);
+        (Uuid outputPackageId, List<string> metadataFileList, List<string> packageFileList) =
+            CreatePackage(PackageType.ArchivalInformationPackage);
 
         string rootDir = outputPackageId + "/";
 
@@ -103,9 +110,13 @@ public class InformationPackageCreatorTest
 
         // Files in total, including the directories implicit tested by subentries (above)
         packageFileList.Count.Should().Be(22);
+
+        // All files in the package (except the metadata file itself) should be described in its metadata:
+        List<string> packageFilesExpectedInMetadata = GetPackageItemsExpectedInMetadata(packageFileList);
+        metadataFileList.Should().BeEquivalentTo(packageFilesExpectedInMetadata);
     }
 
-    private (Uuid outputPackageId, List<string> packageFileList) CreatePackage(PackageType packageType)
+    private (Uuid, List<string>, List<string>) CreatePackage(PackageType packageType)
     {
         using var disposableDirectory = new DisposableDirectory(_tmpDirectory);
         DirectoryInfo processingDirectory = disposableDirectory.Get().CreateSubdirectory("processing");
@@ -122,7 +133,11 @@ public class InformationPackageCreatorTest
             _ => null
         };
 
-        return (archive.OutputDiasPackage.Id, GetFileListFromTarArchive(packageFilePath)); // NB! UUID-writeout (unit testing)
+        return (
+            archive.OutputDiasPackage.Id, // NB! UUID-writeout (unit testing)
+            GetFileListFromMetadata(archive.OutputDiasPackage),
+            GetFileListFromTarArchive(packageFilePath)
+        );
     }
 
     private static InformationPackageCreator InformationPackageCreator()
@@ -136,6 +151,13 @@ public class InformationPackageCreatorTest
         return new InformationPackageCreator(metadataFilesCreator, statusEventHandler, siardMetadataFileHelper);
     }
 
+    private static List<string> GetFileListFromMetadata(OutputDiasPackage outputDiasPackage)
+    {
+        List<FileDescription> fileDescriptions = outputDiasPackage.ArchiveMetadata.FileDescriptions;
+
+        return fileDescriptions.Select(f => (outputDiasPackage.Id + "/" + f.Name).Replace('\\', '/')).ToList();
+    }
+
     private static List<string> GetFileListFromTarArchive(string tarArchiveFilePath)
     {
         var fileList = new List<string>();
@@ -146,5 +168,20 @@ public class InformationPackageCreatorTest
         tarArchive.ListContents();
 
         return fileList;
+    }
+
+    private static List<string> GetPackageItemsExpectedInMetadata(List<string> packageFileList)
+    {
+        return packageFileList.Where(item => IsNotADirectoryItem(item) && IsNotTheMetadataFile(item)).ToList();
+
+        bool IsNotADirectoryItem(string fileListItem)
+        {
+            return Path.HasExtension(fileListItem);
+        }
+
+        bool IsNotTheMetadataFile(string fileListItem)
+        {
+            return !fileListItem.EndsWith("dias-mets.xml");
+        }
     }
 }
