67337417153737bf792908a6428673c105b8ff5c
Jørgen Tellnes <jorgen@arkitektum.no>
2026-02-17 16:50:30 +0100

    Support uncombined path segments as testadata helper method input

--- src/Arkivverket.Arkade.Core.Tests/UnitTestUtilities/TestData.cs
+++ src/Arkivverket.Arkade.Core.Tests/UnitTestUtilities/TestData.cs
@@ -10,9 +10,9 @@ public static class TestData
 
     public static DirectoryInfo Directory() => new(TestDataDirectoryPath);
 
-    public static DirectoryInfo Directory(string testDataDirectorySubPath) =>
-        new(Path.Combine(TestDataDirectoryPath, testDataDirectorySubPath));
+    public static DirectoryInfo Directory(params string[] testDataDirectorySubPathSegments) =>
+        new(Path.Combine([TestDataDirectoryPath, .. testDataDirectorySubPathSegments]));
 
-    public static FileInfo File(string testDataDirectorySubPath) =>
-        new(Path.Combine(TestDataDirectoryPath, testDataDirectorySubPath));
+    public static FileInfo File(params string[] testDataDirectorySubPathSegments) =>
+        new(Path.Combine([TestDataDirectoryPath, .. testDataDirectorySubPathSegments]));
 }
