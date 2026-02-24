75d7391dd8afcc18e69130df836220a4d9f4d2cc
Jørgen Tellnes <jorgen@arkitektum.no>
2026-02-24 14:26:12 +0100

    Use testdata helper

--- src/Arkivverket.Arkade.Core.Tests/Base/Archives/ArchiveFactoryTest.cs
+++ src/Arkivverket.Arkade.Core.Tests/Base/Archives/ArchiveFactoryTest.cs
@@ -3,6 +3,7 @@ using System.IO;
 using Arkivverket.Arkade.Core.Base;
 using Arkivverket.Arkade.Core.Base.Archives;
 using Arkivverket.Arkade.Core.Logging;
+using Arkivverket.Arkade.Core.Tests.UnitTestUtilities;
 using Arkivverket.Arkade.Core.Util;
 using FluentAssertions;
 using Xunit;
@@ -11,18 +12,15 @@ namespace Arkivverket.Arkade.Core.Tests.Base.Archives;
 
 public class ArchiveFactoryTest : IDisposable
 {
-    private static readonly string TestDataDirectory =
-        Path.Combine(Environment.CurrentDirectory, "TestData");
-
     private static readonly ArchiveFactory ArchiveFactory = new(new TarCompressionUtility(), new StatusEventHandler());
 
-    public ArchiveFactoryTest() => ArkadeProcessingArea.Establish(TestDataDirectory);
+    public ArchiveFactoryTest() => ArkadeProcessingArea.Establish(TestData.DirectoryPath());
     public void Dispose() => ArkadeProcessingArea.Destroy();
 
     [Fact]
     public void CreateSiardArchiveTest()
     {
-        var siardArchiveFile = new FileInfo(Path.Combine(TestDataDirectory,
+        var siardArchiveFile = new FileInfo(TestData.DirectoryPath(
             "Archives", "Siard", "extraction", "dbptk.siard"));
         Archive siardArchive = ArchiveFactory.Create(siardArchiveFile, ArchiveType.Siard);
         siardArchive.GetType().Should().Be(typeof(SiardArchive));
@@ -34,7 +32,7 @@ public class ArchiveFactoryTest : IDisposable
     [Fact]
     public void CreateSiardArchiveFromDiasTest()
     {
-        var siardArchiveInDiasTarFile = new FileInfo(Path.Combine(TestDataDirectory,
+        var siardArchiveInDiasTarFile = new FileInfo(TestData.DirectoryPath(
             "Archives", "Siard", "diasPackage", "841c0a18-7308-4407-b421-3efaa420d891.tar"));
         Archive siardArchiveFromDias = ArchiveFactory.Create(siardArchiveInDiasTarFile, ArchiveType.Siard);
         siardArchiveFromDias.GetType().Should().Be(typeof(SiardArchive));
@@ -46,7 +44,7 @@ public class ArchiveFactoryTest : IDisposable
     [Fact]
     public void CreateNoark5ArchiveTest()
     {
-        var noark5ArchiveDirectory = new DirectoryInfo(Path.Combine(TestDataDirectory,
+        var noark5ArchiveDirectory = new DirectoryInfo(TestData.DirectoryPath(
             "Archives", "Noark5", "extraction"));
         Archive noark5Archive = ArchiveFactory.Create(noark5ArchiveDirectory, ArchiveType.Noark5);
         noark5Archive.GetType().Should().Be(typeof(Noark5Archive));
@@ -58,7 +56,7 @@ public class ArchiveFactoryTest : IDisposable
     [Fact]
     public void CreateNoark5ArchiveFromDiasTest()
     {
-        var noark5ArchiveInDiasTarFile = new FileInfo(Path.Combine(TestDataDirectory,
+        var noark5ArchiveInDiasTarFile = new FileInfo(TestData.DirectoryPath(
             "Archives", "Noark5", "diasPackage", "4b73981c-1fab-4d4c-91e7-fcc6a3bc057f.tar"));
         Archive noark5ArchiveFromDias = ArchiveFactory.Create(noark5ArchiveInDiasTarFile, ArchiveType.Noark5);
         noark5ArchiveFromDias.GetType().Should().Be(typeof(Noark5Archive));
@@ -70,7 +68,7 @@ public class ArchiveFactoryTest : IDisposable
     [Fact]
     public void CreateNoark4ArchiveTest()
     {
-        var noark4ArchiveDirectory = new DirectoryInfo(Path.Combine(TestDataDirectory,
+        var noark4ArchiveDirectory = new DirectoryInfo(TestData.DirectoryPath(
             "Archives", "Noark4", "extraction"));
         Archive noark4Archive = ArchiveFactory.Create(noark4ArchiveDirectory, ArchiveType.Noark4);
         noark4Archive.GetType().Should().Be(typeof(Noark4Archive));
@@ -82,7 +80,7 @@ public class ArchiveFactoryTest : IDisposable
     [Fact]
     public void CreateNoark4ArchiveFromDiasTest()
     {
-        var noark4ArchiveInDiasTarFile = new FileInfo(Path.Combine(TestDataDirectory,
+        var noark4ArchiveInDiasTarFile = new FileInfo(TestData.DirectoryPath(
             "Archives", "Noark4", "diasPackage", "ffb1fda0-5b13-478f-9e4a-d68e8a944399.tar"));
         Archive noark4ArchiveFromDias = ArchiveFactory.Create(noark4ArchiveInDiasTarFile, ArchiveType.Noark4);
         noark4ArchiveFromDias.GetType().Should().Be(typeof(Noark4Archive));
@@ -94,7 +92,7 @@ public class ArchiveFactoryTest : IDisposable
     [Fact]
     public void CreateNoark3ArchiveTest()
     {
-        var noark3ArchiveDirectory = new DirectoryInfo(Path.Combine(TestDataDirectory,
+        var noark3ArchiveDirectory = new DirectoryInfo(TestData.DirectoryPath(
             "Archives", "Noark3", "extraction"));
         Archive noark3Archive = ArchiveFactory.Create(noark3ArchiveDirectory, ArchiveType.Noark3);
         noark3Archive.GetType().Should().Be(typeof(Noark3Archive));
@@ -106,7 +104,7 @@ public class ArchiveFactoryTest : IDisposable
     [Fact]
     public void CreateNoark3ArchiveFromDiasTest()
     {
-        var noark3ArchiveInDiasTarFile = new FileInfo(Path.Combine(TestDataDirectory,
+        var noark3ArchiveInDiasTarFile = new FileInfo(TestData.DirectoryPath(
             "Archives", "Noark3", "diasPackage", "8851c420-80e0-4681-b838-8eeb542d46f1.tar"));
         Archive noark3ArchiveFromDias = ArchiveFactory.Create(noark3ArchiveInDiasTarFile, ArchiveType.Noark3);
         noark3ArchiveFromDias.GetType().Should().Be(typeof(Noark3Archive));
@@ -118,7 +116,7 @@ public class ArchiveFactoryTest : IDisposable
     [Fact]
     public void CreateSpecializedSystemArchiveTest()
     {
-        var specializedSystemArchiveDirectory = new DirectoryInfo(Path.Combine(TestDataDirectory,
+        var specializedSystemArchiveDirectory = new DirectoryInfo(TestData.DirectoryPath(
             "Archives", "SpecializedSystem", "extraction"));
         Archive specializedSystemArchive =
             ArchiveFactory.Create(specializedSystemArchiveDirectory, ArchiveType.SpecializedSystem);
@@ -131,7 +129,7 @@ public class ArchiveFactoryTest : IDisposable
     [Fact]
     public void CreateSpecializedSystemArchiveFromDiasTest()
     {
-        var specializedSystemArchiveInDiasTarFile = new FileInfo(Path.Combine(TestDataDirectory,
+        var specializedSystemArchiveInDiasTarFile = new FileInfo(TestData.DirectoryPath(
             "Archives", "SpecializedSystem", "diasPackage", "bf193afe-4483-4481-b457-e9ba4f19681c.tar"));
         Archive specializedSystemArchiveFromDias =
             ArchiveFactory.Create(specializedSystemArchiveInDiasTarFile, ArchiveType.SpecializedSystem);
