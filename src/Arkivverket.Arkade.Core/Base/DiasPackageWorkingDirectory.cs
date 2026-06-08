c55d1c9ba93eed07b3cd171e11397db0c30a1809
Jørgen Tellnes <jorgen@arkitektum.no>
2026-06-08 12:37:17 +0200

    Remove dead external-content-directory scaffolding

--- src/Arkivverket.Arkade.Core/Base/DiasPackageWorkingDirectory.cs
+++ src/Arkivverket.Arkade.Core/Base/DiasPackageWorkingDirectory.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Reflection;
 using Arkivverket.Arkade.Core.Base.Archives;
@@ -13,62 +12,16 @@ namespace Arkivverket.Arkade.Core.Base
         private static readonly ILogger Log = Serilog.Log.ForContext(MethodBase.GetCurrentMethod().DeclaringType);
 
         private readonly ArkadeDirectory _root;
-        //private readonly ArkadeDirectory _externalContentDirectory;
 
         public DiasPackageWorkingDirectory(DirectoryInfo root)
         {
             _root = new ArkadeDirectory(root);
         }
-        //public DiasPackageWorkingDirectory(DirectoryInfo root, DirectoryInfo externalContentDirectory)
-        //{
-        //    _root = new ArkadeDirectory(root);
-        //    if (externalContentDirectory != null)
-        //    {
-        //        _externalContentDirectory = new ArkadeDirectory(externalContentDirectory);
-        //        Log.Debug("Setting up working directory with external content directory: " + externalContentDirectory.FullName);
-        //    }
-        //}
-
-        //public static DiasPackageWorkingDirectory FromArchiveFile()
-        //{
-        //    return FromExternalDirectory(null);
-        //}
-
-        ///// <summary>
-        ///// Initializes a new working directory for this archive using the timestamp as the identifier. An empty folder structure is created on disk.
-        ///// </summary>
-        ///// <param name="externalContentDirectory">optional external content directory</param>
-        ///// <returns></returns>
-        //internal static DiasPackageWorkingDirectory FromExternalDirectory(DirectoryInfo externalContentDirectory)
-        //{
-        //    if (ArkadeProcessingArea.WorkDirectory == null)
-        //        throw new IOException(Resources.ExceptionMessages.ArkadeProcessAreaNotSet);
-
-        //    string dateString = DateTime.Now.ToString("yyyyMMddHHmmss");
-        //    var rootDirectory = new DirectoryInfo(
-        //        Path.Combine(ArkadeProcessingArea.WorkDirectory.FullName, dateString)
-        //    );
-
-        //    var workingDirectory = new DiasPackageWorkingDirectory(rootDirectory, externalContentDirectory);
-        //    workingDirectory.CreateAllFolders();
-        //    return workingDirectory;
-        //}
 
         public ArkadeDirectory Root()
         {
             return _root;
         }
-
-        /// <summary>
-        /// This directory contains the archive files. Can be an external content directory located outside of Arkade's work directory.
-        /// </summary>
-        /// <returns></returns>
-        //public ArkadeDirectory Content()
-        //{
-        //    if (HasExternalContentDirectory())
-        //        return _externalContentDirectory;
-        //    return _root.WithSubDirectory("content");
-        //}
 
         /// <summary>
         /// This is the local content directory inside Arkade's work directory. Writing of content files (like the addml.xml) should done within this directory.
@@ -103,11 +56,6 @@ namespace Arkivverket.Arkade.Core.Base
                 RepositoryOperations().Create();
             ContentWorkDirectory().Create();
         }
-
-        //public bool HasExternalContentDirectory()
-        //{
-        //    return _externalContentDirectory != null;
-        //}
 
         public long GetSize()
         {
